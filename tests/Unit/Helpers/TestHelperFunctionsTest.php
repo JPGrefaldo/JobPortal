d9093cf41552156c1bff5177fc70249bb342fd44
bryan <bryan@castingcallsamerica.com>
2018-04-09 13:04:18 -0700

    Whoops duplicate

--- tests/Unit/Helpers/TestHelperFunctionsTest.php
+++ tests/Unit/Helpers/TestHelperFunctionsTest.php
@@ -5,12 +5,6 @@ use Tests\TestCase;
 class TestHelperFunctionsTest extends TestCase
 {
     /** @test */
-    public function stripe_non_numeric_characters()
-    {
-        $this->assertEquals('15555555555', stripNonNumeric('+1 (555) 555-5555'));
-    }
-
-    /** @test */
     public function create_test_us_phone_number()
     {
         $number = createTestUSPhoneNumber();
