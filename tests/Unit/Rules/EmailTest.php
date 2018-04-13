00adfce40ce84a93f2306d9d9a6da2f2b29d5ea1
Leoncio Cesar L. Polestico <leoncio.cesar@hjugroup.io>
2018-04-13 15:50:20 +0800

    Rename class

--- tests/Unit/Rules/EmailTest.php
+++ tests/Unit/Rules/EmailTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class YouTubeTest extends TestCase
+class EmailTest extends TestCase
 {
     /** @test */
     public function valid_email()
