33bb526376d8715a2fefa8db75c92aa377a13d5d
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-07-12 16:46:46 +0800

    remove unused use case

--- tests/Browser/EndorsementTest.php
+++ tests/Browser/EndorsementTest.php
@@ -19,29 +19,6 @@ class EndorsementTest extends DuskTestCase
     /**
      * @test
      */
-    public function a_user_can_endorse_another_user()
-    {
-        $endorser = factory(User::class)->create();
-        $endorsee = factory(User::class)->create();
-
-        $projectJob = factory(ProjectJob::class)->create();
-
-        $this->browse(function (Browser $browser) use ($endorser, $endorsee, $projectJob) {
-            $browser->loginAs($endorser)
-                ->visit(new ProjectJobIndexPage($projectJob))
-                ->endorse($endorsee);
-        });
-
-        $this->assertDatabaseHas('endorsements', [
-            'project_job_id' => $projectJob->id,
-            'endorser_id'    => $endorser->id,
-            'endorsee_id'    => $endorsee->id,
-        ]);
-    }
-
-    /**
-     * @test
-     */
     public function an_endorsee_can_ask_an_endorsement_from_an_endorser()
     {
         $endorsee      = factory(User::class)->create();
