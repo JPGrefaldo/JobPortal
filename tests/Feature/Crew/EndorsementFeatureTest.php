745a16a5153e288f66c761d05416e4960d6e4cdc
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-07-20 15:25:16 +0800

    WIP

--- tests/Feature/Crew/EndorsementFeatureTest.php
+++ tests/Feature/Crew/EndorsementFeatureTest.php
@@ -127,14 +127,29 @@ class EndorsementFeatureTest extends TestCase
     {
         // given
         // an endorsement
+        $endorsement = factory(Endorsement::class)->create();
         // 2 crew members
+        $endorser1 = factory(Crew::class)->states('withRole')->create();
+        $user1 = $endorser1->user;
+        $endorser2 = factory(Crew::class)->states('withRole')->create();
+        $user2= $endorser2->user;
 
         // when
         // the crew members open the link
+        $response1 = $this->actingAs($user1)
+            ->get(route('endorsement.edit', ['endorsement' => $endorsement]));
 
+        $response2 = $this->actingAs($user2)
+            ->get(route('endorsement.edit', ['endorsement' => $endorsement]));
+
+        // dump($response1->getContent());
+        // dump($response2->getContent());
+
+            // dump($user1->toArray());
+            // dump($user2->toArray());
         // then
         // endorsement rank becomes 2
-        $this->assert();
+        $this->assertCount(2, Endorsement::all()->toArray());
     }
 
     /**
@@ -194,7 +209,7 @@ class EndorsementFeatureTest extends TestCase
 
         // then
         // endorser is forbidden
-        $this->assert();
+        // $this->assert();
     }
 
     /**
@@ -202,62 +217,62 @@ class EndorsementFeatureTest extends TestCase
      */
     public function endorsed_crew_are_ranked_based_on_how_many_endorsers_they_get()
     {
-        $this->withoutExceptionHandling();
-        // given
-        $endorsement = factory(Endorsement::class)->create();
+        // $this->withoutExceptionHandling();
+        // // given
+        // $endorsement = factory(Endorsement::class)->create();
 
-        $endorsee = $endorsement->crew;
-        $endorser = factory(Crew::class)->create();
-        $position = $endorsement->crewPosition->position;
-        $endorsee->askEndorsementFrom($endorser->email, $position);
+        // $endorsee = $endorsement->crew;
+        // $endorser = factory(Crew::class)->create();
+        // $position = $endorsement->crewPosition->position;
+        // $endorsee->askEndorsementFrom($endorser->email, $position);
 
-        // given
-        // users that have asked endorsements
-        // endorsee 1 asks 1 endorsement
-        $position = factory(Position::class)->create();
+        // // given
+        // // users that have asked endorsements
+        // // endorsee 1 asks 1 endorsement
+        // $position = factory(Position::class)->create();
 
-        $endorser1 = factory(Crew::class)->create();
+        // $endorser1 = factory(Crew::class)->create();
 
-        $endorsee1 = factory(Crew::class)->create();
+        // $endorsee1 = factory(Crew::class)->create();
 
-        $endorsement1 = $endorsee1->askEndorsementFrom($endorser1->user->email, $position);
+        // $endorsement1 = $endorsee1->askEndorsementFrom($endorser1->user->email, $position);
 
-        $endorser1->acceptEndorsement($endorsement1);
+        // $endorser1->acceptEndorsement($endorsement1);
 
-        // endorsee 2 asks 2 endorsements
-        $endorser2 = factory(Crew::class)->create();
-        $endorser3 = factory(Crew::class)->create();
+        // // endorsee 2 asks 2 endorsements
+        // $endorser2 = factory(Crew::class)->create();
+        // $endorser3 = factory(Crew::class)->create();
 
-        $endorsee2    = factory(Crew::class)->create();
-        $endorsement2 = $endorsee2->askEndorsementFrom($endorser2->user->email, $position);
-        $endorsement3 = $endorsee2->askEndorsementFrom($endorser3->user->email, $position);
+        // $endorsee2    = factory(Crew::class)->create();
+        // $endorsement2 = $endorsee2->askEndorsementFrom($endorser2->user->email, $position);
+        // $endorsement3 = $endorsee2->askEndorsementFrom($endorser3->user->email, $position);
 
-        $endorser2->acceptEndorsement($endorsement1);
-        $endorser3->acceptEndorsement($endorsement1);
+        // $endorser2->acceptEndorsement($endorsement1);
+        // $endorser3->acceptEndorsement($endorsement1);
 
-        // endorsee 3 asks 3 endorsements
-        $endorser4 = factory(Crew::class)->create();
-        $endorser5 = factory(Crew::class)->create();
-        $endorser6 = factory(Crew::class)->create();
+        // // endorsee 3 asks 3 endorsements
+        // $endorser4 = factory(Crew::class)->create();
+        // $endorser5 = factory(Crew::class)->create();
+        // $endorser6 = factory(Crew::class)->create();
 
-        $endorsee3    = factory(Crew::class)->create();
-        $endorsement4 = $endorsee3->askEndorsementFrom($endorser4->user->email, $position);
-        $endorsement5 = $endorsee3->askEndorsementFrom($endorser5->user->email, $position);
-        $endorsement6 = $endorsee3->askEndorsementFrom($endorser6->user->email, $position);
+        // $endorsee3    = factory(Crew::class)->create();
+        // $endorsement4 = $endorsee3->askEndorsementFrom($endorser4->user->email, $position);
+        // $endorsement5 = $endorsee3->askEndorsementFrom($endorser5->user->email, $position);
+        // $endorsement6 = $endorsee3->askEndorsementFrom($endorser6->user->email, $position);
 
-        $endorser4->acceptEndorsement($endorsement4);
-        $endorser5->acceptEndorsement($endorsement5);
-        $endorser6->acceptEndorsement($endorsement6);
-        // and those endorsements are approved
+        // $endorser4->acceptEndorsement($endorsement4);
+        // $endorser5->acceptEndorsement($endorsement5);
+        // $endorser6->acceptEndorsement($endorsement6);
+        // // and those endorsements are approved
 
-        // when
-        // I visit index page of a position I must see endorsements sorted by most voted to least voted
-        $response = $this->getJson("/positions/{$position->id}/endorsements");
+        // // when
+        // // I visit index page of a position I must see endorsements sorted by most voted to least voted
+        // $response = $this->getJson("/positions/{$position->id}/endorsements");
 
-        // dd(collect(json_decode($response->getContent()))->pluck('id'));
+        // // dd(collect(json_decode($response->getContent()))->pluck('id'));
 
-        // then
-        $this->assertEquals($endorsements->ids, $project->topEndorsees);
+        // // then
+        // $this->assertEquals($endorsements->ids, $project->topEndorsees);
     }
 
 }
