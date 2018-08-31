c548c48ddc95bf5e3de84de9e50a343c8b209e23
Bryan Heath <bryan@castingcallsamerica.com>
2018-08-31 15:10:37 -0700

    Combined GUI elements

---
+++ resources/views/_parts/componets/single-card-button-page.blade.php
@@ -0,0 +1,20 @@
+<div class="container max-w-xl flex justify-center items-center">
+    <div class="w-full md:w-1/2 float-left mb-3 md:mb-0 px-4">
+        @include('_parts.messagebox')
+        <div class="bg-white shadow-md rounded">
+            <form method="post" action="{{ $cardRoute }}">
+                <div class="p-8">
+                    {{ $cardContent }}
+                    <div class="pt-6">
+                        @csrf
+                        <input type="submit"
+                               class="btn-rounded btn-blue block font-header uppercase text-sm p-4 w-full text-center"
+                               value="{{ $cardButtonValue }}"
+                        >
+                    </div>
+                </div>
+            </form>
+            {{ $cardUnderContent ?? '' }}
+        </div>
+    </div>
+</div>
