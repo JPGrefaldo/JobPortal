c6cb9f0df33c084fb1da59cdf3028a923c22f15d
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-09-03 11:08:35 +0800

    update relation add test

---
+++ tests/Unit/Models/UserNotificationSettingTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\User;
+use App\Models\UserNotificationSetting;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserNotificationSettingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setup();
+
+        $this->user = factory(User::class)->create();
+        $this->userNotificationSetting =
+            factory(UserNotificationSetting::class)
+                ->create(['user_id' => $this->user->id]);
+    }
+
+    /**
+     * @test
+     */
+    public function user()
+    {
+        $this->assertEquals(
+            $this->user->email,
+            $this->userNotificationSetting->user->email
+        );
+    }
+}
