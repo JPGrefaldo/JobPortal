767b3ff4ffda8946ce002beef5aacbf9238ce2cf
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-09-03 08:23:57 +0800

    add test

---
+++ tests/Unit/Models/CrewReelTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Crew;
+use App\Models\CrewReel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CrewReelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setup();
+
+        $this->crew = factory(Crew::class)->create();
+        $this->crewReel = factory(CrewReel::class)->create([
+            'crew_id' => $this->crew->id
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function crew()
+    {
+        $this->assertEquals($this->crew->id, $this->crewReel->crew->id);
+    }
+}
