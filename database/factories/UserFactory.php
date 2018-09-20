4dbbde12743396664003acae22c750f25689be83
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-09-20 17:23:21 +0800

    pass test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,3 +25,9 @@ $factory
     ->afterCreatingState(App\Models\User::class, 'withCrewRole', function ($user, $faker) {
         $user->roles()->attach(Role::where('name', Role::CREW)->first());
     });
+
+$factory
+    ->state(App\Models\User::class, 'withProducerRole', [])
+    ->afterCreatingState(App\Models\User::class, 'withProducerRole', function ($user, $faker) {
+        $user->roles()->attach(Role::where('name', Role::PRODUCER)->first());
+    });
