3025d8464ad1f47714d6ed656bebfe17239fe7c6
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-09-27 04:10:41 +0800

    finish crew positions index

--- resources/views/crew/position/index.blade.php
+++ resources/views/crew/position/index.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
 
-    <main class="float-left w-full py-md md:py-lg">
         <div class="container">
             @include('_parts.pages.page-title', ['pageTitle' => 'Positions'])
-            <table>
-                @foreach ($positions as $position)
+            @foreach ($positions as $position)
 
-                    <tr>
-                        <td>
-                            <a href="{{ route('crew_position.show', $position) }}">{{ $position->name }}</a>
-                        </td>
-                    </tr>
+                <div class="flex justify-between mb-2 rounded bg-grey-light shadow h-16 items-center px-2">
+                    <a class="" href="{{ route('crew_position.show', $position) }}">{{ $position->name }}</a>
+                    <div class="flex items-center">
+                        <div class="mr-2">Endorsers: {{ auth()->user()->crew->endorsements->count() }}</div>
+                        @if (auth()->user()->crew->hasPosition($position))
+                            <a class="inline-block border border-red rounded py-2 px-4 bg-red hover:bg-red-dark text-white" href="#">
+                                Leave
+                            </a>
+                        @else
+                            <a class="inline-block border border-blue rounded py-2 px-4 bg-blue hover:bg-blue-dark text-white" href="#">
+                                Apply
+                            </a>
+                        @endif
+                    </div>
+                </div>
 
-                @endforeach
-            </table>
+            @endforeach
         </div>
-    <main>
 
 @endsection
