da277fe6ab4c7f49b0a8fbc59308f620fa9fd43b
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-10-24 04:46:49 +0800

    remove uuid persistence bind route to hash ids instead

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->uuid('uuid');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')
