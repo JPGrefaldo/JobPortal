8e143faf1ccb55d09ef7613cbf2f4200d17403e6
Mike Kevin Castro <mikekevin.castro@hjugroup.io>
2018-10-27 02:44:05 +0800

    unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('hash_id')
+                ->unique()
                 ->nullable();
             $table->string('first_name');
             $table->string('last_name');
