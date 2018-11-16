7772cd13b47dc738e33401c32714bff44b35f24f
Mike Castro <mikekevin.castro@hjugroup.io>
2018-11-17 01:20:03 +0800

    add tests

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Project;
+use App\Models\ProjectJob;
+use App\Models\ProjectType;
+use App\Models\RemoteProject;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function type()
+    {
+        // given
+        $projectType = factory(ProjectType::class)->create();
+
+        // when
+        $project = factory(Project::class)->create([
+            'project_type_id' => $projectType->id,
+        ]);
+
+
+        // then
+        $this->assertEquals(
+            $projectType->name,
+         $project->type->name
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function remotes()
+    {
+        // given
+        $project = factory(Project::class)->create();
+
+        // when
+        $remoteProject = factory(RemoteProject::class, 3)->create([
+            'project_id' => $project->id,
+        ]);
+
+        // then
+        $this->assertCount(3, $project->remotes);
+    }
+
+    /**
+     * @test
+     */
+    public function jobs()
+    {
+        // given
+        $project = factory(Project::class)->create();
+
+        // when
+        $jobs = factory(ProjectJob::class, 3)->create([
+            'project_id' => $project->id,
+        ]);
+
+        // then
+        $this->assertCount(3, $project->jobs);
+    }
+}
