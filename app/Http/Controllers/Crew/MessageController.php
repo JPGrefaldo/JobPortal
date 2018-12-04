357f132281be879c679c76af0d2acba50205ca75
hju-mikekevin-castro <38409882+hju-mikekevin-castro@users.noreply.github.com>
2018-12-05 01:04:52 +0800

    crew can reply to producer (#84)

---
+++ app/Http/Controllers/Crew/MessageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Crew;
+
+use App\Http\Controllers\Controller;
+
+class MessageController extends Controller
+{
+    public function store()
+    {
+        return 'Producer messaged successfully.';
+    }
+}
