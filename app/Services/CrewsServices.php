ca222c012a96b45eab89a990ea5d0f9a9aeabcd7
Vaneza Aumento <vaneza@millstrading.local>
2018-12-04 15:53:03 +1100

    fixed crew profile template. currently trying to fix adding of social profiles.

--- app/Services/CrewsServices.php
+++ app/Services/CrewsServices.php
@@ -24,6 +24,7 @@ class CrewsServices
      */
     public function processCreate(array $data, User $user)
     {
+
         if (isset($data['photo'])) {
             $crew = $this->create(
                 array_only($data, ['bio']),
