a47b559233b922b0cb31d744bef8479a8c02abc3
Leoncio Cesar L. Polestico <leoncio.cesar@hjugroup.io>
2018-12-05 09:34:38 +0800

    Use tymon/jwt-auth as the auth API driver

---
+++ tests/Feature/API/FetchUserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use App\Models\User;
+use Tests\Support\SeedDatabaseAfterRefresh;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FetchUserTest extends TestCase
+{
+    use RefreshDatabase,
+        SeedDatabaseAfterRefresh;
+
+    const URI = 'api/user';
+
+    /** @test */
+    public function user()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user, 'api')->getJson(self::URI);
+
+        $response->assertSuccessful()
+            ->assertJsonStructure([
+                'id',
+                'first_name',
+                'last_name'
+            ]);
+    }
+
+    /** @test */
+    public function unauthorize_guest()
+    {
+        $response = $this->getJson(self::URI);
+
+        $response->assertStatus(401);
+    }
+}
