31e5cc106ab56fa77f43d5c009b7ffa730bf0916
Mike Castro <mikekevin.castro@hjugroup.io>
2018-12-12 17:13:51 +0800

    create component fix default layout delete messenger footer

---
+++ resources/assets/js/components/MessagesDashboardComponent.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="flex flex-col h-full">
+        <!-- top bar -->
+        <div class="flex h-12 bg-grey-light">
+            <div class="w-1/5 border-b border-r border-black flex items-center justify-center px-2">
+                <button class="fa fa-search mr-2"></button>
+                <input type="text" class="hidden rounded-full bg-white flex-1 px-3 mr-2" placeholder="Search">
+                <div class="bg-white rounded-full h-5 flex-1 pl-3 mr-2 text-grey">
+                    Search
+                </div>
+                <button class="fa fa-edit"></button>
+            </div>
+            <div class="w-4/5 text-md border-black border-b font-bold flex justify-center items-center">
+                Titanic: Leonardo DiCarpio
+                <!-- TODO: right chevron goes here that links to the current recipient -->
+                <!-- might have to add logic depending on the role -->
+            </div>
+        </div>
+        <!-- main -->
+        <div class="flex h-full">
+            <!-- left pane -->
+            <div class="flex w-1/5 border-r border-black">
+                <div class="bg-grey-dark overflow-auto">
+                    <button class="uppercase flex items-center justify-center mb-2 m-1 bg-blue hover:bg-blue-dark text-white font-bold h-10 w-10 rounded">
+                        pa
+                    </button>
+                    <button class="uppercase flex items-center justify-center mb-2 m-1 bg-blue hover:bg-blue-dark text-white font-bold h-10 w-10 rounded">
+                        r
+                    </button>
+                    <div class="uppercase flex items-center justify-center mb-2 m-1 bg-blue-dark hover:bg-blue-dark text-white font-bold h-10 w-10 rounded">
+                        t
+                    </div>
+                    <button class="uppercase flex items-center justify-center mb-2 m-1 bg-blue hover:bg-blue-dark text-white font-bold h-10 w-10 rounded">
+                        z
+                    </button>
+                </div>
+                <!-- threads -->
+                <div class="flex-1 overflow-auto bg-white">
+                    <!-- thread -->
+                    <div class="flex items-center justify-center p-2 bg-grey-light">
+                        <div class="h-10 w-10 rounded-full bg-white background-missing-avatar"></div>
+                        <div class="p-2 flex-1">
+                            <div class="mb-1">
+                                Leonardo DiCarpio
+                            </div>
+                            <p class="text-xs">
+                                You: Awesome! You ar...
+                            </p>
+                        </div>
+                    </div>
+                    <!-- thread -->
+                    <div class="flex items-center justify-center p-2">
+                        <div class="h-10 w-10 rounded-full bg-white background-missing-avatar"></div>
+                        <div class="p-2 flex-1">
+                            <div class="mb-1">
+                                Kate Winslet
+                            </div>
+                            <p class="text-xs">
+                                I am happy.
+                            </p>
+                        </div>
+                </div>
+                </div>
+            </div>
+            <!-- conversation -->
+            <div class="w-4/5 bg-white flex flex-col p-4">
+                <!-- recipient message -->
+                <div class="flex items-center justify-center mb-4">
+                    <div class="mr-4 border h-10 w-10 rounded-full bg-white background-missing-avatar"></div>
+                    <div class="rounded-lg bg-grey-light p-3 max-w-md">
+                        Impedit sit cumque ut voluptatem voluptatem est ea esse. Odit ipsa molestiae sint exercitationem quia tempora. Eaque non ipsum dolores reiciendis dicta. Eligendi minima quam dolorem quo voluptatem maxime qui.
+                    </div>
+                    <div class="flex-1"></div>
+                </div>
+                <!-- recipient message -->
+                <div class="flex items-center justify-center mb-4">
+                    <div class="mr-4 border h-10 w-10 rounded-full bg-white background-missing-avatar"></div>
+                    <div class="rounded-lg bg-grey p-3 max-w-md">
+                        Eum ut quibusdam modi dolores voluptas. Assumenda possimus assumenda et voluptatum. Facere soluta rerum nostrum eaque. Maxime consequuntur velit et et perspiciatis alias iste.
+                    </div>
+                    <div class="flex-1"></div>
+                </div>
+                <!-- sender message -->
+                <div class="flex mb-4">
+                    <div class="flex-1"></div>
+                    <div class="rounded-lg text-white bg-blue p-3 max-w-md">
+                        Eveniet et neque mollitia sed. Rem rem quis dolores ea est. Tempora sit tempore asperiores necessitatibus.
+                    </div>
+                </div>
+                <!-- sender message -->
+                <div class="flex mb-4">
+                    <div class="flex-1"></div>
+                    <div class="rounded-lg text-white bg-blue-dark p-3 max-w-md">
+                        Awesome! You are the best!
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- bottom bar -->
+        <div class="flex h-12 w-screen">
+            <div class="w-1/5 flex border-t border-r border-black">
+                <button class="bg-blue w-1/2 flex justify-center items-center">
+                    producer
+                </button>
+                <button class="bg-green w-1/2 flex justify-center items-center">
+                    crew
+                </button>
+            </div>
+            <div class="w-4/5 bg-grey-light flex justify-between items-center p-3">
+                <input type="text" class="w-64 rounded-full flex-1 mr-2 px-3" placeholder="Aa">
+                <button class="fa fa-paper-plane mr-1"></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    import Form from '../form.js';
+
+    export default {
+        name: "MessagesDashboardComponent",
+        props: {
+        },
+        data() {
+            return {
+
+            }
+        },
+
+        methods: {
+        }
+    }
+</script>
+
