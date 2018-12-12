5ffec7902db2906f5a4a8e52f2df3e1e4bcb92ed
Mike Castro <mikekevin.castro@hjugroup.io>
2018-12-12 11:29:55 +0800

    cleaup

--- resources/views/layouts/default_layout.blade.php
+++ resources/views/layouts/default_layout.blade.php
@@ -1,5 +1,5 @@
 @include('_parts.header.header')
-<body class="bg-grey-lighter font-body h-screen flex flex-col">
+<body class="bg-grey-lighter font-body @if (Route::current()->getName() === 'messages')) h-screen @else min-h-screen @endif flex flex-col">
 
 @if(Auth::check())
     @include('_parts.navbar.navbar-logged-in')
@@ -7,14 +7,17 @@
     @include('_parts.navbar.navbar-not-logged-in')
 @endif
 
+@if (Route::current()->getName() === 'messages')
     @yield('content')
-{{-- <main class="float-left w-full @if (Route::current()->getName() !== 'messages')) py-lg @endif flex-1" id="content">
-    @include('_parts.messagebox')
-    @yield('content')
-</main> --}}
+@else
+    <main class="float-left w-full @if (Route::current()->getName() !== 'messages')) py-lg @endif flex-1" id="content">
+        @include('_parts.messagebox')
+        @yield('content')
+    </main>
+@endif
 
 @if (Route::current()->getName() !== 'messages')
-    @include('_parts.footer.footer')
-@else
     @include('_parts.footer.messenger')
+@else
+    @include('_parts.footer.footer')
 @endif
