66d1a9d5d87e4e6440e2bdb59cf5bccf25cd63bd
Mike Castro <mikekevin.castro@hjugroup.io>
2018-12-13 17:17:59 +0800

    WIP

--- resources/views/messages-dashboard.blade.php
+++ resources/views/messages-dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default_layout')
 
 @section('content')
-    <messages-dashboard></messages-dashboard>
+    <messages-dashboard :roles="{{ $roles }}"></messages-dashboard>
 @endsection
