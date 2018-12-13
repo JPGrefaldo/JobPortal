091dc2d5ef431d77bc6407de1eebf67034d98863
Vaneza Aumento <vaneza@hjugroup.io>
2018-12-14 09:15:13 +1100

    Added / in the beginning of the URLs

--- resources/views/_parts/footer/footer-javascript.blade.php
+++ resources/views/_parts/footer/footer-javascript.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
-<script type="text/javascript" src="{{ mix('js/all.js') }}"></script>
-<script type="text/javascript" src="{{ mix('js/scripts.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/all.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/scripts.js') }}"></script>
 @stack('javascripts')
--- resources/views/_parts/header/header-css.blade.php
+++ resources/views/_parts/header/header-css.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 
-<link rel="stylesheet" href="{{ mix('css/styles.css') }}">
-<link rel="stylesheet" href="{{ mix('css/main.css') }}">
-<link rel="stylesheet" href="{{ mix('css/plugins.css') }}">
+<link rel="stylesheet" href="{{ mix('/css/styles.css') }}">
+<link rel="stylesheet" href="{{ mix('/css/main.css') }}">
+<link rel="stylesheet" href="{{ mix('/css/plugins.css') }}">
 @stack('css')
