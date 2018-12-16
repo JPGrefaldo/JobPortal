35d9f1236a7704565d5477bde9fefe1d1d5821cc
Vaneza Aumento <vaneza@hjugroup.io>
2018-12-17 09:56:35 +1100

    Applied changes from recent scan review

--- tests/Unit/Models/SocialLinkTypeTest.php
+++ tests/Unit/Models/SocialLinkTypeTest.php
@@ -15,12 +15,15 @@ class SocialLinkTypeTest extends TestCase
     public function get_slug_attribute()
     {
         $this->assertEquals('imdb',
-            with(new SocialLinkType(['name'=>'IMDB']))->slug);
+            with(new SocialLinkType(['name'=>'IMDB']))->slug
+        );
 
         $this->assertEquals('google_plus',
-            with(new SocialLinkType(['name'=>'Google Plus']))->slug);
+            with(new SocialLinkType(['name'=>'Google Plus']))->slug
+        );
 
         $this->assertEquals('facebook',
-            with(new SocialLinkType(['name'=>'Facebook']))->slug);
+            with(new SocialLinkType(['name'=>'Facebook']))->slug
+        );
     }
 }
