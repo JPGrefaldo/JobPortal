0fb16858f54db7fe7a3e1f49e9f6a8590281ccff
Vaneza Aumento <vaneza@hjugroup.io>
2018-12-17 10:21:21 +1100

    Added code to show saved social links on crew profile

--- resources/views/crew/profile/profile-index.blade.php
+++ resources/views/crew/profile/profile-index.blade.php
@@ -14,7 +14,9 @@
                    class="text-grey w-full mb-2 text-sm md:text-right float-right"><i class="fas fa-pencil-alt mr-2"></i>Edit profile</a>
                 <div class="card float-left md:flex mb-8">
                     <div class="md:w-1/4 md:pr-8 text-center">
-                        <img src="{{ url('photos/avatar.png') }}" class="rounded" alt="" />
+                        <img src="{{ url('photos/avatar.png') }}"
+                             class="rounded"
+                             alt="Avatar" />
                         <ul class="list-reset py-4">
                              @foreach($socialLinkTypes as $key => $socialLinkType)
                                  @if(isset($socialLinkType->crew[0]))
