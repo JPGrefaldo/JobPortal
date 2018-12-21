de8820738f948828bf1a20449e8cf6520fbbf4e8
Mike Castro <mikekevin.castro@hjugroup.io>
2018-12-21 16:21:08 +0800

    refactor

--- app/Http/Controllers/MessagesDashboardController.php
+++ app/Http/Controllers/MessagesDashboardController.php
@@ -2,24 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Cmgmyr\Messenger\Models\Message;
-use Cmgmyr\Messenger\Models\Thread;
-
 class MessagesDashboardController extends Controller
 {
     public function index()
     {
-        $threads = Thread::with('messages')->get();
-
-        $messages = Message::all();
-
         $roles = auth()->user()->roles->pluck('name');
 
         return view(
             'messages-dashboard',
             compact(
-                'threads',
-                'messages',
                 'roles'
             )
         );
