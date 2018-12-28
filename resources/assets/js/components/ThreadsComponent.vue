fafe7f9b89f5528fa95b24d5413099e5149507a9
Mike Castro <mikekevin.castro@hjugroup.io>
2018-12-28 09:39:27 +0800

    make v-for first attribute

--- resources/assets/js/components/ThreadsComponent.vue
+++ resources/assets/js/components/ThreadsComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex-1 overflow-auto bg-white">
         <!-- thread -->
-        <button class="flex items-center justify-center p-2 hover:bg-grey-light w-full"
-            v-for="thread in threads" :key="thread.id"
+        <button v-for="thread in threads" :key="thread.id"
+            class="flex items-center justify-center p-2 hover:bg-grey-light w-full"
             @click="$emit('onClickSetThread', thread)">
             <div class="h-10 w-10 rounded-full bg-white background-missing-avatar border"></div>
             <div class="p-2 flex-1">
