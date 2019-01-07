2dbe8189a33107b8931a3f05162dfa04480ea0fc
Mike Castro <mikekevin.castro@hjugroup.io>
2019-01-07 09:30:15 +0800

    Put ::title first and class in the next line

--- resources/assets/js/components/ThreadsComponent.vue
+++ resources/assets/js/components/ThreadsComponent.vue
@@ -5,7 +5,8 @@
             :key="thread.id"
             class="flex items-center justify-center p-2 hover:bg-grey-light w-full"
             @click="$emit('onClickSetThread', thread)">
-            <div class="h-10 w-10 rounded-full bg-white background-missing-avatar border" :title="thread.subject"></div>
+            <div :title="thread.subject"
+                class="h-10 w-10 rounded-full bg-white background-missing-avatar border"></div>
             <div class="p-2 flex-1">
                 <div class="mb-1 truncate w-32">
                     {{ thread.subject }}
