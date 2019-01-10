458e153263048436bc0cfbbc89fde27aac2b2767
Vaneza Aumento <vaneza@hjugroup.io>
2019-01-10 20:28:08 +1100

    -[WIP] get uploaded image via s3 bucket

--- resources/views/crew/profile/profile-index.blade.php
+++ resources/views/crew/profile/profile-index.blade.php
@@ -16,7 +16,7 @@
                     <div class="md:w-1/4 md:pr-8 text-center">
                         @if (isset($user->crew->photo))
                             <div class="flex h-none bg-grey-light items-center justify-center text-center border border-grey-light w-full pb-full rounded relative"
-                                 style="background: url('/storage/{{ $user->crew->photo }}'); background-size: cover;">
+                                 style="background: url('https://s3-us-west-2.amazonaws.com/test.crewcalls.info/{{ $user->crew->photo }}'); background-size: cover;">
                             </div>
                         @else
                             <img src="{{ url('photos/avatar.png') }}"
@@ -81,7 +81,7 @@
                             </a>
                         @endif
 
-                        @if (isset($user->crew->resumes))
+                        @if (isset($user->crew->resumes->where('general', 1)->first()->url))
                             <a href="/storage/{{$user->crew->resumes->where('general', 1)->first()->url}}"
                                target="_blank"
                                class="border md:w-1/2 flex items-center overflow-hidden rounded md:ml-2">
