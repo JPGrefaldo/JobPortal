ccb002f14f80015130e8a4c7487cf50aeb6fed45
Mike Castro <mikekevin.castro@hjugroup.io>
2019-01-13 16:49:25 +0800

    create pending flag message

---
+++ app/Http/Controllers/PendingFlagMessageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PendingFlagMessage;
+use Illuminate\Http\Request;
+
+class PendingFlagMessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PendingFlagMessage  $pendingFlagMessage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PendingFlagMessage $pendingFlagMessage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PendingFlagMessage  $pendingFlagMessage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PendingFlagMessage $pendingFlagMessage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PendingFlagMessage  $pendingFlagMessage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PendingFlagMessage $pendingFlagMessage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PendingFlagMessage  $pendingFlagMessage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PendingFlagMessage $pendingFlagMessage)
+    {
+        //
+    }
+}
