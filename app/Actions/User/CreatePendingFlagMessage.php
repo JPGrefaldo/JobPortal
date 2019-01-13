5793ea9462ba4187b7dd41b5b09afc5b5bb0493b
Mike Castro <mikekevin.castro@hjugroup.io>
2019-01-13 17:35:25 +0800

    create createPendingFlagMessage action

---
+++ app/Actions/User/CreatePendingFlagMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Actions\User;
+
+use App\Mail\PendingMessageFlagged;
+use App\Models\PendingFlagMessage;
+use App\Models\User;
+use Cmgmyr\Messenger\Models\Message;
+use Illuminate\Support\Facades\Mail;
+
+class CreatePendingFlagMessage
+{
+    /**
+     * @param User $user
+     * @param string $password
+     */
+    public function execute($data)
+    {
+        $message = Message::find($data['message_id']);
+
+        PendingFlagMessage::create([
+            'message_id' => $message->id,
+            'reason' => $data['reason'],
+        ]);
+
+        Mail::to('admin@admin.com')->send(new PendingMessageFlagged($message));
+    }
+}
--- app/Models/PendingFlagMessage.php
+++ app/Models/PendingFlagMessage.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class PendingFlagMessage extends Model
 {
+    /**
+     * The protected attributes
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
     public function message()
     {
         return $this->belongsTo(Message::class);
