8d8e0c9b4a00f078e851d25e2c138ed7963c3bff
Mike Castro <mikekevin.castro@hjugroup.io>
2019-01-13 17:13:46 +0800

    create pending flag message `message` relationship

--- app/Models/PendingFlagMessage.php
+++ app/Models/PendingFlagMessage.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Cmgmyr\Messenger\Models\Message;
 use Illuminate\Database\Eloquent\Model;
 
 class PendingFlagMessage extends Model
 {
-    //
+    public function message()
+    {
+        return $this->belongsTo(Message::class);
+    }
 }
