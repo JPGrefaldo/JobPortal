d9b196358d39e86bb50069678639692b6bb21085
Leoncio Cesar L. Polestico <leoncio.cesar@hjugroup.io>
2019-01-14 10:56:40 +0800

    Fix indexOf error in buildParams

--- resources/assets/js/plugins/axios.js
+++ resources/assets/js/plugins/axios.js
@@ -1,29 +1,35 @@
 import axios from 'axios'
 import jsonpAdapter from 'axios-jsonp';
 
+window.jsonpCallback = (params) => {}
+
 axios.interceptors.request.use(request => {
     if (! /^\/?api/.test(request.url)) {
         return
     }
 
+    let url = request.url
     let method = request.method
     let adapter = new jsonpAdapter({
-        params: {
-            method: 'options',
-            headers: {
-                common: {
-                    'X-Requested-With': 'XMLHttpRequest',
-                    'Access-Control-Allow-Origin': 'http://champ-crewcalls.test',
-                    'Access-Control-Request-Method': method.toUpperCase(),
-                    'Content-Type': 'application/json'
-                }
-            }
-        }
+        method,
+        url,
+        callbackParamName: 'jsonpCallback'
     })
 
 
+
+
     Object.assign(request, {
+        method: 'options',
         adapter,
+        headers: {
+            common: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Access-Control-Allow-Origin': 'http://champ-crewcalls.test',
+                'Access-Control-Request-Method': method.toUpperCase(),
+                'Content-Type': 'application/json'
+            }
+        }
     })
 
     console.log(request)
