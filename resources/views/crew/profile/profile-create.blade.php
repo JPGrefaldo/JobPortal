0d7b19680b7fea0fef9cc4fc016a1f58262f406c
Vaneza Aumento <vaneza@hjugroup.io>
2019-01-18 17:26:59 +1100

    -[WIP] added form for each work positions

--- resources/views/crew/profile/profile-create.blade.php
+++ resources/views/crew/profile/profile-create.blade.php
@@ -143,6 +143,45 @@
                             </label>
                         </div>
                     </div>
+                    <form>
+                    <div class="p-2 md:p-4 border-t-2 border-grey-lighter bg-white">
+                        <div class="py-2">
+                            <div class="mb-2">
+                                <textarea class="form-control w-full h-64" placeholder="Biography" name="bio"></textarea>
+                            </div>
+                        </div>
+                        <div class="border-t-2 border-grey-lighter py-4">
+                            <div class="md:flex">
+                                <div class="md:w-1/3 pr-8">
+                                    <h3 class="text-md font-header mb-2 md:mb-0">General resume</h3>
+                                </div>
+                                <div class="md:w-2/3">
+                                    <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
+                                    <input id="resume"
+                                           type="file"
+                                           name="resume"
+                                           class="invisible">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-t-2 border-grey-lighter py-4">
+                            <div class="md:flex">
+                                <div class="md:w-1/3 pr-8">
+                                    <h3 class="text-md font-header mt-2 mb-2 md:mb-0">General reel</h3>
+                                </div>
+                                <div class="md:w-2/3">
+                                    <input type="text" name="reel_link" class="form-control bg-light w-64 mr-2 mb-2 md:mb-0" placeholder="Add link"> or <div class="md:w-2/3">
+                                        <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
+                                        <input id="resume"
+                                               type="file"
+                                               name="resume"
+                                               class="invisible">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
                     @endforeach
                     <div class="pt-8 pb-4 text-right border-t-2 border-grey-lighter">
                         <a href="#" class="text-grey bold mr-4 hover:text-green">Cancel</a>
