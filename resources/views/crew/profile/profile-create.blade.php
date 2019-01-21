a273f34c3e0c90967b8cb1ffd829d75c5dd7f3a6
Vaneza Aumento <vaneza@hjugroup.io>
2019-01-22 01:51:40 +1100

    -added form to positions -added new scss file for gear toggle format

--- resources/views/crew/profile/profile-create.blade.php
+++ resources/views/crew/profile/profile-create.blade.php
@@ -142,46 +142,71 @@
                                 <div class="control-indicator"></div>
                             </label>
                         </div>
-                    </div>
-                    <form>
-                    <div class="p-2 md:p-4 border-t-2 border-grey-lighter bg-white">
-                        <div class="py-2">
-                            <div class="mb-2">
-                                <textarea class="form-control w-full h-64" placeholder="Biography" name="bio"></textarea>
-                            </div>
-                        </div>
-                        <div class="border-t-2 border-grey-lighter py-4">
-                            <div class="md:flex">
-                                <div class="md:w-1/3 pr-8">
-                                    <h3 class="text-md font-header mb-2 md:mb-0">General resume</h3>
+                        <div class="position-form">
+                            <div class="p-2 md:p-4 border-t-2 border-grey-lighter bg-white">
+                                <div class="py-2">
+                                    <div class="mb-2">
+                                        <textarea class="form-control w-full h-64" placeholder="Biography" name="bio"></textarea>
+                                    </div>
                                 </div>
-                                <div class="md:w-2/3">
-                                    <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
-                                    <input id="resume"
-                                           type="file"
-                                           name="resume"
-                                           class="invisible">
+                                <div class="border-t-2 border-grey-lighter py-4">
+                                    <div class="md:flex">
+                                        <div class="md:w-1/3 pr-8">
+                                            <h3 class="text-md font-header mb-2 md:mb-0">General resume</h3>
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
+                                            <input id="resume"
+                                                   type="file"
+                                                   name="resume"
+                                                   class="invisible">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="border-t-2 border-grey-lighter py-4">
-                            <div class="md:flex">
-                                <div class="md:w-1/3 pr-8">
-                                    <h3 class="text-md font-header mt-2 mb-2 md:mb-0">General reel</h3>
+                                <div class="border-t-2 border-grey-lighter py-4">
+                                    <div class="md:flex">
+                                        <div class="md:w-1/3 pr-8">
+                                            <h3 class="text-md font-header mt-2 mb-2 md:mb-0">General reel</h3>
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <input type="text" name="reel_link" class="form-control bg-light w-64 mr-2 mb-2 md:mb-0" placeholder="Add link"> or
+                                            <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
+                                            <input id="resume"
+                                                   type="file"
+                                                   name="resume"
+                                                   class="invisible">
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="md:w-2/3">
-                                    <input type="text" name="reel_link" class="form-control bg-light w-64 mr-2 mb-2 md:mb-0" placeholder="Add link"> or <div class="md:w-2/3">
-                                        <label for="resume" class="btn-outline text-green inline-block" >Upload file</label>
-                                        <input id="resume"
-                                               type="file"
-                                               name="resume"
-                                               class="invisible">
+                                <div class="border-t-2 border-grey-lighter py-4">
+                                    <div class="md:flex">
+                                        <div class="md:w-1/3 pr-8">
+                                            <h3 class="text-md font-header mt-2 mb-2 md:mb-0">Gear</h3>
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <div class="container">
+                                                <label class="switch" for="checkbox">
+                                                    <input type="checkbox" id="checkbox-gear" />
+                                                    <div class="slider round"></div>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="md:flex">
+                                        <div class="md:w-1/3 pr-8">
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <h3 class="text-md font-header mt-2 mb-2 md:mb-0">What gear do you have for this position?</h3>
+                                            <br>
+                                            <div class="mb-2">
+                                                <textarea class="form-control w-full h-64" placeholder="Your gear" name="gear"></textarea>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </form>
                     @endforeach
                     <div class="pt-8 pb-4 text-right border-t-2 border-grey-lighter">
                         <a href="#" class="text-grey bold mr-4 hover:text-green">Cancel</a>
