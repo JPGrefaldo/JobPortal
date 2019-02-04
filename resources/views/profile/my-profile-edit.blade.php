1ff342f89f355bb7ca44c8c5a8c5e39acabbbd2c
Rafa Rafael <12.12afa3l@gmail.com>
2019-02-04 12:01:10 +0800

    Change current user display from  full name to nickname

--- resources/views/profile/my-profile-edit.blade.php
+++ resources/views/profile/my-profile-edit.blade.php
@@ -40,7 +40,7 @@
                 <div class="card mb-8">
                     <div class="w-full mb-6">
                         <h3 class="text-blue-dark font-semibold text-md md:text-lg mb-1 font-header">
-                            {{ $user->first_name }} {{ $user->last_name }}</h3>
+                            {{ $user->nickname }}</h3>
                     </div>
 
                      {{ Form::open(array('route' => 'profile-update', 'files' => true)) }}
