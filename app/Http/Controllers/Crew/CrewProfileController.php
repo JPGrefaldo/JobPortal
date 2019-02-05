65e749ce315b6840eefe87251c98ca3ea672adf0
Rafa Rafael <rafa@hjugroup.io>
2019-02-05 12:52:23 +0800

    Refactor validation for resume url

--- app/Http/Controllers/Crew/CrewProfileController.php
+++ app/Http/Controllers/Crew/CrewProfileController.php
@@ -24,7 +24,21 @@ class CrewProfileController extends Controller
         $user = Auth::user()->load([
             'crew'
         ]);
-        $resume_url = $user->crew->resumes->where( 'general',1)->first()->url;
+        
+        $resume_url = '';
+        if (isset(
+            $user->crew
+                 ->resumes
+                 ->where( 'general',1)
+                 ->first()
+                 ->url
+        )){
+            $resume_url = $user->crew
+                               ->resumes
+                               ->where( 'general',1)
+                               ->first()
+                               ->url;
+        }
 
         return view('crew.profile.profile-index', [
             'user' => $user,
