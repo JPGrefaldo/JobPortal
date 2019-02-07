d934099036e0dc3731e873c3a8f3496650003254
Rafa Rafael <rafa@hjugroup.io>
2019-02-07 12:30:24 +0800

    Refactor test methods

--- tests/Feature/Manager/ManagerFeatureTest.php
+++ tests/Feature/Manager/ManagerFeatureTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Manager;
 
 use App\Models\User;
+use App\Models\Manager;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\Support\SeedDatabaseAfterRefresh;
@@ -18,42 +19,26 @@ class ManagerFeatureTest extends TestCase
      */
     public function user_can_add_manager()
     {
-        $user = $this->createUser();
-        $email = ["manager@email.com"];
+        $subordinate = $this->createUser();
+        $manager = $this->createUser([
+                        'email' => 'manager@email.com'
+                   ]) ;
 
-        $this->actingAs($user)
+        $this->actingAs($subordinate)
              ->get(route('account.manager'));
 
-        $response = $this->actingAs($user)
-                         ->post(route('account.manager'), $email)
-                         ->assertStatus(200);
-    }
+        $response = $this->actingAs($subordinate)
+                         ->post(route('account.manager'), [
+                             'email' => $manager->email
+                         ]);
 
-    /**
-     * @test
-     * @covers \App\Http\Controllers\Account\AccountManagerController::store
-     */
-    public function user_can_edit_manager()
-    {
-        
-    }
+        $response->assertRedirect(route('account.manager'));
 
-    /**
-     * @test
-     * @covers \App\Http\Controllers\Account\AccountManagerController::destroy
-     */
-    public function user_can_delete_manager()
-    {
-        
+        $this->assertContains([
+            'manager_id' => 2
+        ],
+            $manager->refresh()
+                    ->toArray()
+        );
     }
-
-    /**
-     * @test
-     * @covers \App\Http\Controllers\Account\AccountManagerController::destroy
-     */
-    public function manager_will_be_deleted_when_user_is_deleted()
-    {
-        
-    }
-
 }
