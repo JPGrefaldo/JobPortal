cbdc31932955886bb38d65885314ebc2b6478a61
Rafa Rafael <rafa@hjugroup.io>
2019-02-07 13:24:50 +0800

    Create additional test method

--- tests/Feature/Manager/ManagerFeatureTest.php
+++ tests/Feature/Manager/ManagerFeatureTest.php
@@ -15,6 +15,73 @@ class ManagerFeatureTest extends TestCase
 
     /**
      * @test
+     * @covers \App\Http\Controllers\Account\AccountManagerController::index
+     */
+    public function show_manager_email()
+    {
+        $subordinate = $this->createUser();
+        $manager = $this->createUser([
+                        'email' => 'manager@email.com'
+                   ]);
+
+        $this->actingAs($subordinate)
+             ->get(route('account.manager'));
+
+        $response = $this->actingAs($subordinate)
+                         ->post(route('account.manager'), [
+                             'email' => $manager->email
+                         ]);
+
+        $response->assertRedirect(route('account.manager'));
+
+        $this->assertEquals('manager@email.com', $manager->email);
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\Account\AccountManagerController::store
+     */
+    public function cannnot_add_manager_using_unregistered_email()
+    {
+        $subordinate = $this->createUser();
+        $manager = [
+            'email' => 'manager@email.com'
+        ];
+
+       $this->actingAs($subordinate)
+             ->get(route('account.manager'));
+
+        $response = $this->actingAs($subordinate)
+                         ->post(route('account.manager'), $manager);
+
+        $response->assertRedirect(route('account.manager'));
+
+        $response->assertSessionHasErrors('unregistered_email');
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\Account\AccountManagerController::store
+     */
+    public function cannnot_add_manager_using_own_email()
+    {
+        $user = $this->createUser();
+
+        $this->actingAs($user)
+             ->get(route('account.manager'));
+
+        $response = $this->actingAs($user)
+                         ->post(route('account.manager'), [
+                             'email' => $user->email
+                         ]);
+
+        $response->assertRedirect(route('account.manager'));
+
+        $response->assertSessionHasErrors('own_email');
+    }
+
+    /**
+     * @test
      * @covers \App\Http\Controllers\Account\AccountManagerController::store
      */
     public function user_can_add_manager()
@@ -22,7 +89,7 @@ class ManagerFeatureTest extends TestCase
         $subordinate = $this->createUser();
         $manager = $this->createUser([
                         'email' => 'manager@email.com'
-                   ]) ;
+                   ]);
 
         $this->actingAs($subordinate)
              ->get(route('account.manager'));
@@ -36,6 +103,43 @@ class ManagerFeatureTest extends TestCase
 
         $this->assertContains([
             'manager_id' => 2
+        ],
+            $manager->refresh()
+                    ->toArray()
+        );
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\Account\AccountManagerController::store
+     */
+    public function user_can_update_manager()
+    {
+        $subordinate = $this->createUser();
+        $manager = $this->createUser([
+                        'email' => 'manager@email.com'
+                   ]);
+        $manager2 = $this->createUser([
+                            'email' => 'manager2@email.com'
+                    ]);
+
+        $this->actingAs($subordinate)
+             ->get(route('account.manager'));
+
+        $response = $this->actingAs($subordinate)
+                         ->post(route('account.manager'), [
+                             'email' => $manager->email
+                         ]);
+
+        $response->assertRedirect(route('account.manager'));
+
+        $response = $this->actingAs($subordinate)
+                         ->post(route('account.manager'), [
+                             'email' => $manager2->email
+                         ]);
+
+        $this->assertContains([
+            'manager_id' => 3
         ],
             $manager->refresh()
                     ->toArray()
