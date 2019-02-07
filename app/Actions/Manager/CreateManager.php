16240e4b7216d333e067ee39454939c097f76ddf
Rafa Rafael <rafa@hjugroup.io>
2019-02-07 10:15:10 +0800

    Refactor to have a single responsibility

--- app/Actions/Manager/CreateManager.php
+++ app/Actions/Manager/CreateManager.php
@@ -3,37 +3,18 @@
 namespace App\Actions\Manager;
 
 use App\Models\Manager;
-use App\Models\Role;
-use App\Models\Rules\UserRules;
-use App\Models\User;
-use App\Utils\FormatUtils;
 
 class CreateManager
 {
     /**
-     * @param string $email
-     * @param User $user
+     * @param string $manager_id
+     * @param string $subordinate_id
      */
-    public function execute($email, $user)
+    public function execute($managerId, $subordinateId)
     {
-        $email = FormatUtils::email($email);
-
-        if ($manager = User::where('email', $email)->first()){
-            
-            $oldData = Manager::where('subordinate_id', $user->id)->first();
-
-            if($oldData->id)
-            {
-                return $oldData->update([
-                    'manager_id' => $manager->id,
-                ]);
-            }
-
-            return Manager::create([
-                'manager_id' => $manager->id,
-                'subordinate_id' => $user->id,
-                'role_id' => Role::getRoleIdByName(Role::MANAGER),
-            ]);
-        }
+        return Manager::create([
+            'manager_id' => $managerId,
+            'subordinate_id' => $subordinateId
+        ]);
     }
 }
