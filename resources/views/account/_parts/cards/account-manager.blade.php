18a685fccf1d3bf8d25603042b989210d4cf88b7
Rafa Rafael <rafa@hjugroup.io>
2019-02-08 09:26:57 +0800

    Add data attribute and refactor

--- resources/views/account/_parts/cards/account-manager.blade.php
+++ resources/views/account/_parts/cards/account-manager.blade.php
@@ -7,9 +7,9 @@
     <div class="inline-flex">
         <input class="w-full border border-light-grey max-w-sm p-4 my-4" name="email" placeholder="Email Address of the manager" 
             value="{{ isset($manager->email) ? $manager->email : old('email') }}"/>
-        <button class="bg-red-light hover:bg-grey text-red font-bold max-w-sm p-4 my-4 rounded-r">
+        <a href="#" id="removeManager" data-id="{{! isset($manager->id) ?: $manager->id}}" data-token="{{csrf_token()}}" class="bg-red-light hover:bg-grey text-red font-bold max-w-sm p-4 my-4 rounded-r">
             Remove
-        </button>
+        </a>
     </div>
     @endslot
 
