01bf0bc98b530968e9eaf5d28690c92418f7bec3
Rafa Rafael <rafa@hjugroup.io>
2019-02-11 14:22:02 +0800

    Complete the manager confirmation logic

--- app/Http/Controllers/Manager/ManagerConfirmationController.php
+++ app/Http/Controllers/Manager/ManagerConfirmationController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers\Manager;
 
+use App\Models\Manager;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ManagerConfirmationController extends Controller
 {
-    public function index()
+    public function index($managerId, $subordinateId)
     {
-        //TODO: confirmation takes place
+        $manager = User::where('hash_id', $managerId)->first();
+        $subordinate = User::where('hash_id', $subordinateId)->first();
+
+        $manager = Manager::where([
+            'manager_id'=> $manager->id,
+            'subordinate_id'=> $subordinate->id
+        ])->first();
+
+        if($manager->status == 1) {
+            return redirect('login')->withError('confirmed', 'You already accepted '.$subordinate->fullname.'\'s request');
+        }
+
+        $manager->update([
+            'status' => 1
+        ]);
+
+        return redirect('/login')->with('infoMessage', 'You are now '.$subordinate->fullname.'\'s manager');
     }
 }
