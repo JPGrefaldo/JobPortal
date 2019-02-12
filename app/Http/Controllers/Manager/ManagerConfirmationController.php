377cb63db97f51c9f5a85bbba74032d8a0cd0e8e
Rafa Rafael <rafa@hjugroup.io>
2019-02-12 09:12:53 +0800

    Add resend email method

--- app/Http/Controllers/Manager/ManagerConfirmationController.php
+++ app/Http/Controllers/Manager/ManagerConfirmationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Manager;
 
+use App\Events\ManagerAdded;
+use App\Http\Controllers\Controller;
 use App\Models\Manager;
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ManagerConfirmationController extends Controller
 {
@@ -19,7 +21,7 @@ class ManagerConfirmationController extends Controller
             'subordinate_id'=> $subordinate->id
         ])->first();
 
-        if($manager->status == 1) {
+        if ($manager->status == 1) {
             return redirect(route('login'))->withError('confirmed', 'You already accepted '.$subordinate->fullname.'\'s request');
         }
 
@@ -28,5 +30,23 @@ class ManagerConfirmationController extends Controller
         ]);
 
         return redirect(route('login'))->with('infoMessage', 'You are now '.$subordinate->fullname.'\'s manager');
+    }
+
+    public function resend($manager)
+    {
+        $manager = User::findOrfail($manager);
+        $subordinate = Auth::user();
+
+        if (Manager::where([
+                'manager_id'=> $manager->id,
+                'subordinate_id'=> $subordinate->id
+            ])
+            ->where('status', 0)
+            ->first()
+        ) {
+            event(new ManagerAdded($manager, $subordinate));
+        }
+
+        return back()->with('info', 'Sending confirmation email to the manager');
     }
 }
