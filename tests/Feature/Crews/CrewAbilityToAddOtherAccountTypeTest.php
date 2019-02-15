ff0382dfa9634b28c704c7aa3b9d674e68f5a978
Rafa Rafael <rafa@hjugroup.io>
2019-02-15 12:01:51 +0800

    Add additional test methods

--- tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
+++ tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Crews;
 
 use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Tests\Support\SeedDatabaseAfterRefresh;
 use Tests\TestCase;
 
@@ -11,7 +12,7 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
 {
     use RefreshDatabase, SeedDatabaseAfterRefresh;
 
-    public function test_change_account_type()
+    public function test_changing_account_type()
     {
         $user = $this->createUser();
 
@@ -24,28 +25,50 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
 
     public function test_change_to_crew_account_type()
     {
-        $this->withoutExceptionHandling();
-        
-        $user = $this->createUser();
+        $user = $this->createProducer();
         $user->assignRole(Role::PRODUCER);
 
         $this->actingAs($user)
-             ->get(route('account.change'));
+             ->get(route('account.change-to.crew'));
 
         $this->assertEquals($user->roles->pluck('name')[0], Role::CREW);
     }
 
     public function test_change_to_producer_account_type()
     {
-        $this->withoutExceptionHandling();
-        
-        $user = $this->createUser();
+        $user = $this->createCrew();
         $user->assignRole(Role::CREW);
 
         $this->actingAs($user)
-             ->get(route('account.change'));
+             ->get(route('account.change-to.producer'));
 
         $this->assertEquals($user->roles->pluck('name')[0], Role::PRODUCER);
     }
 
+    public function test_only_producer_can_change_to_crew_account()
+    {   
+        $this->expectException(UnauthorizedException::class);
+        $this->disableExceptionHandling();
+
+        $user = $this->createCrew();
+        $user->assignRole(Role::CREW);
+        
+        $this->actingAs($user)
+             ->get(route('account.change-to.crew'))
+             ->assertStatus(500);   
+    }
+
+    public function test_only_crew_can_change_to_producer_account()
+    {   
+        $this->expectException(UnauthorizedException::class);
+        $this->disableExceptionHandling();
+
+        $user = $this->createProducer();
+        $user->assignRole(Role::PRODUCER);
+        
+        $this->actingAs($user)
+             ->get(route('account.change-to.producer'))
+             ->assertStatus(500);
+        
+    }
 }
