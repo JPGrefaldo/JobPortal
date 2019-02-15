30298827646cd5916a9e8afacad677b3f957abde
Rafa Rafael <rafa@hjugroup.io>
2019-02-15 12:30:51 +0800

    Resolve issues

--- tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
+++ tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
@@ -12,37 +12,24 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
 {
     use RefreshDatabase, SeedDatabaseAfterRefresh;
 
-    public function test_changing_account_type()
-    {
-        $user = $this->createUser();
-
-        $user->assignRole(Role::CREW);
-        $this->assertEquals($user->roles->pluck('name')[0], Role::CREW);
-
-        $user->syncRoles(Role::PRODUCER);
-        $this->assertEquals($user->roles->pluck('name')[0], Role::PRODUCER);
-    }
-
     public function test_change_to_crew_account_type()
     {
         $user = $this->createProducer();
-        $user->assignRole(Role::PRODUCER);
 
         $this->actingAs($user)
              ->get(route('account.change-to.crew'));
 
-        $this->assertEquals($user->roles->pluck('name')[0], Role::CREW);
+        $this->assertTrue($user->hasRole(Role::CREW));
     }
 
     public function test_change_to_producer_account_type()
     {
         $user = $this->createCrew();
-        $user->assignRole(Role::CREW);
 
         $this->actingAs($user)
              ->get(route('account.change-to.producer'));
 
-        $this->assertEquals($user->roles->pluck('name')[0], Role::PRODUCER);
+        $this->assertTrue($user->hasRole(Role::PRODUCER));
     }
 
     public function test_only_producer_can_change_to_crew_account()
@@ -51,7 +38,6 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
         $this->disableExceptionHandling();
 
         $user = $this->createCrew();
-        $user->assignRole(Role::CREW);
         
         $this->actingAs($user)
              ->get(route('account.change-to.crew'))
@@ -64,11 +50,9 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
         $this->disableExceptionHandling();
 
         $user = $this->createProducer();
-        $user->assignRole(Role::PRODUCER);
         
         $this->actingAs($user)
              ->get(route('account.change-to.producer'))
              ->assertStatus(500);
-        
     }
 }
