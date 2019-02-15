5aefd1f8a5e053482385a996381521023b2771b4
Mike Castro <mikekevin.castro@hjugroup.io>
2019-02-15 13:02:55 +0800

    fix test format

--- tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
+++ tests/Feature/Crews/CrewAbilityToAddOtherAccountTypeTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Crews;
 
 use App\Models\Role;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Spatie\Permission\Exceptions\UnauthorizedException;
 use Tests\Support\SeedDatabaseAfterRefresh;
 use Tests\TestCase;
 
@@ -33,26 +32,20 @@ class CrewAbilityToAddOtherAccountTypeTest extends TestCase
     }
 
     public function test_only_producer_can_change_to_crew_account()
-    {   
-        $this->expectException(UnauthorizedException::class);
-        $this->disableExceptionHandling();
-
+    {
         $user = $this->createCrew();
-        
+
         $this->actingAs($user)
              ->get(route('account.change-to.crew'))
-             ->assertStatus(500);   
+             ->assertForbidden();
     }
 
     public function test_only_crew_can_change_to_producer_account()
-    {   
-        $this->expectException(UnauthorizedException::class);
-        $this->disableExceptionHandling();
-
+    {
         $user = $this->createProducer();
-        
+
         $this->actingAs($user)
              ->get(route('account.change-to.producer'))
-             ->assertStatus(500);
+             ->assertForbidden();
     }
 }
