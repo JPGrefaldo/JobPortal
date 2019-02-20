87cf5524d25db0586de2e6b3d3235a9b0807177b
Rafa Rafael <rafa@hjugroup.io>
2019-02-20 11:40:12 +0800

    Refactor to be more redeable and use localStorage.user

--- resources/assets/js/components/messaging/messages/index.vue
+++ resources/assets/js/components/messaging/messages/index.vue
@@ -10,117 +10,36 @@
             <div v-for="message in messages"
                 :key="message.id">
                 <!-- sender message template -->
-                <message-sender  v-if="isSender(message)" :message="message"></message-sender>
+                <cca-recipient-message  v-if="isSender(message)" :message="message"></cca-recipient-message>
                 <!-- recipient message template -->
-                <message-recepient v-else :message="message"></message-recepient>
+                <cca-recipient-message v-else :message="message"></cca-recipient-message>
             </div>
         </div>
     </div>
 </template>
 
 <script type="text/javascript">
-    import { Form, HasError, AlertError } from 'vform';
-    import MessageRecepient from './MessageRecepient.vue'
-    import MessageSender from './MessageSender.vue'
+    import { mapGetters } from 'vuex'
+    import RecipientMessage from './RecipientMessage.vue'
+    import SenderMessage from './SenderMessage.vue'
 
     export default {
+        
         components: {
-            'message-recepient': MessageRecepient,
-            'message-sender': MessageSender
+            'cca-recipient-message': RecipientMessage,
+            'cca-sender-message': SenderMessage
         },
 
-        props: {
-            user: {
-                type: Object,
-                required: true,
-            },
-            role: {
-                type: String,
-                required: true
-            },
-            messages: {
-                type: Array,
-                required: false
-            },
-        },
-
-        data() {
-            return {
-                form: new Form({
-                    message_id: '',
-                    reason: '',
-                }),
-            }
+        computed: {
+            ...mapGetters({
+                messages: 'messages/list',
+            })
         },
 
         methods: {
-            getColorByRole: function (role) {
-                const colorDictionary = {
-                    Producer: [
-                        'bg-blue',
-                        'hover:bg-blue-dark',
-                    ],
-                    Crew: [
-                        'bg-green',
-                        'hover:bg-green-dark',
-                    ]
-                }
-
-                return colorDictionary[role]
-            },
-
             isSender: function (message) {
-                return message.user_id === this.user.id
+                return message.user_id === localStorage.user.id
             },
-
-            onClickRequestFlag: function (message) {
-                this.requestFlag(message);
-            },
-
-            async requestFlag (message) {
-                const result = await this.displayRequestFlagForm();
-
-                if (!result.value) {
-                    return
-                }
-
-                const response = await this.submitRequestFlagForm(message, result)
-
-                this.displaySuccess(response);
-            },
-
-            displayRequestFlagForm: function () {
-                return this.$swal({
-                    title: 'Report this message',
-                    text: 'Help us understand what\'s happening with this message.',
-                    input: 'textarea',
-                    inputPlaceholder: 'Enter reason',
-                    showCancelButton: true,
-                    cancelButtonColor: '#3085d6',
-                    confirmButtonColor: '#d33',
-                    confirmButtonText: 'Flag message'
-                });
-            },
-
-            submitRequestFlagForm: function (message, result) {
-                this.form.message_id = message.id;
-                this.form.reason = result.value;
-
-                const response = this.form.post('/pending-flag-messages')
-
-                this.form.message_id = '';
-                this.form.reason = '';
-
-                return response;
-            },
-
-            displaySuccess: function (response) {
-                this.$swal({
-                    title: '',
-                    text: response.data.message,
-                    type: 'success',
-                });
-            }
         }
     }
 </script>
