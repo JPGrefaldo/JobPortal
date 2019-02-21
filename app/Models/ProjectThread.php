098c3e71b8ef28119bbffe57e020e983357c4792
Rafa Rafael <rafa@hjugroup.io>
2019-02-21 11:24:03 +0800

    Add relationship to the pivot table

--- app/Models/ProjectThread.php
+++ app/Models/ProjectThread.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Role;
+use App\Models\Project;
+use Cmgmyr\Messenger\Models\Thread;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ProjectThread extends Pivot
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function threads()
+    {
+        return $this->belongsToMany(Project::class, 'project_id')
+                    ->using(Thread::class)
+                    ->as('threads')
+                    ->withPivot('threads');
+    }
+
+    public function isNotAdmin(User $user)
+    {
+        return !! $user->hasRole(Role::ADMIN);
+    }
 }
