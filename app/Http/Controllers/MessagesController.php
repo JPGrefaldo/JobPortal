dfdcb56ca27b6daad61c003c2a84484f5c62785d
Rafa Rafael <rafa@hjugroup.io>
2019-02-22 12:13:59 +0800

    Add methods for storing message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Messenger\CreateMessage;
 use App\Http\Resources\MessageResource;
+use App\Models\Role;
 use Cmgmyr\Messenger\Models\Thread;
 use Illuminate\Http\Request;
 
@@ -28,6 +30,29 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return $request->all();
+        $thread = Thread::findOrFail($request->thread);
+        $sender = $request->sender;
+        $body = $request->message;
+
+        $participant = $thread->users()
+                              ->where('user_id', $sender)
+                              ->first();
+
+        // Optional
+        if(! isset($participant)) {
+            return response()->json([
+                'error' => 'You are not included to this thread'
+            ]);
+        }
+
+        if($participant->hasRole(Role::ADMIN)) {
+            return response()->json([
+                'error' => 'Admin is not allowed to participate on this thread'
+            ]);
+        }
+
+        $message = app(CreateMessage::class)->execute($thread->id, $sender, $body);
+        return response()->json(compact('message'));
     }
 }
