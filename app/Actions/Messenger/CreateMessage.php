f6c28b2cd539890f9612e9205f485edb2c98cb57
Rafa Rafael <rafa@hjugroup.io>
2019-02-22 12:13:59 +0800

    Add methods for storing message

---
+++ app/Actions/Messenger/CreateMessage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Actions\Messenger;
+
+use Cmgmyr\Messenger\Models\Message;
+
+class CreateMessage
+{
+    /**
+     * @param string $managerId
+     * @param string $subordinateId
+     * @return \Cmgmyr\Messenger\Models\Message
+     */
+    public function execute($thread, $user, $body)
+    {
+        return Message::create([
+                    'thread_id' => $thread,
+                    'user_id' => $user,
+                    'body' => $body
+               ]);
+    }
+}
