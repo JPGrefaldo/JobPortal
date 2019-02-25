cf214c7027367a8afbaa73134260856084eb8e52
Rafa Rafael <rafa@hjugroup.io>
2019-02-25 10:52:11 +0800

    Refactor, rename and fix some test bugs

--- resources/assets/js/components/messenger/messages/index.vue
+++ resources/assets/js/components/messenger/messages/index.vue
@@ -1,21 +1,21 @@
 <template>
     <!-- conversation -->
     <div class="w-4/5 bg-white flex flex-col p-4">
-            <div v-if="messages.length === 0"
-                class="text-grey-dark">
-                <div class="fa fa-arrow-left mr-2"></div>
-                Select a thread
-            </div>
-            <div v-else>
-                <div v-for="message in messages"
-                    :key="message.id" >
-                    <!-- sender message template -->
-                    <cca-sender-message  v-if="isSender(message)" :message="message" :role="role"></cca-sender-message>
-                    <!-- recipient message template -->
-                    <cca-recipient-message v-else :message="message"></cca-recipient-message>
-                </div>
+        <div v-if="messages.length === 0"
+            class="text-grey-dark">
+            <div class="fa fa-arrow-left mr-2"></div>
+            Select a thread
+        </div>
+        <div v-else>
+            <div v-for="message in messages"
+                :key="message.id" >
+                <!-- sender message template -->
+                <cca-sender-message  v-if="isSender(message)" :message="message" :role="role"></cca-sender-message>
+                <!-- recipient message template -->
+                <cca-recipient-message v-else :message="message"></cca-recipient-message>
             </div>
         </div>
+    </div>
 </template>
 
 <script type="text/javascript">
