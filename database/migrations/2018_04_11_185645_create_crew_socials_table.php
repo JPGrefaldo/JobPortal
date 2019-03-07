cf37d55b0cf0df65fd8794120c971452c2080986
Bryan Heath <bryan@castingcallsamerica.com>
2019-03-07 10:05:01 -0800

    Fix migration name

--- database/migrations/2018_04_11_185645_create_crew_socials_table.php
+++ database/migrations/2018_04_11_185645_create_crew_socials_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCrewSocialTable extends Migration
+class CreateCrewSocialsTable extends Migration
 {
     /**
      * Run the migrations.
