64f7abd727dbae57bf353479121b69cd73bbc5b5
Bryan Heath <bryan@castingcallsamerica.com>
2019-03-07 13:11:46 -0800

    Added test and minor refactor of Approve and Disapprove message

---
+++ tests/Unit/Actions/Admin/DisapproveFlagMessageTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Services\Admin;
+
+use App\Actions\Admin\DisapproveFlagMessage;
+use App\Models\PendingFlagMessage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Support\SeedDatabaseAfterRefresh;
+use Tests\TestCase;
+
+class DisapproveFlagMessageTest extends TestCase
+{
+    /**
+     * @var \App\Actions\Admin\DisapproveFlagMessage
+     */
+    protected $service;
+
+    use RefreshDatabase, SeedDatabaseAfterRefresh;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->service = app(DisapproveFlagMessage::class);
+    }
+
+    /**
+     * @test
+     * @covers \App\Actions\Admin\DisapproveFlagMessage::execute
+     */
+    public function execute()
+    {
+        $pendingFlagMessage = factory(PendingFlagMessage::class)->create();
+
+        $this->assertNull($pendingFlagMessage->disapproved_at);
+
+        $this->service->execute($pendingFlagMessage);
+        $pendingFlagMessage->refresh();
+
+        $this->assertNotNull($pendingFlagMessage->disapproved_at);
+    }
+}
