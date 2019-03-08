711a71235f2e3036a3822e5dbb16a9ddcf1eae5b
Rafa Rafael <rafa@hjugroup.io>
2019-03-08 23:42:30 +0800

    Add vue components draft and redesign the frontend

---
+++ resources/assets/js/components/project/create/index.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="container bg-white shadow-md rounded p-4">
+
+        <label class="block text-grey-darker font-bold mb-6">
+            Tutorial Videos/How it Works
+        </label>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Project Title
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text">
+        </div>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Production company name (or your name if individual)
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text">
+        </div>
+
+        <div class="mb-6">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-state">
+                Project type
+            </label>
+            <div class="relative">
+                <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey" id="grid-state">
+                    <option v-for="projectType in projectTypes" :key="projectType.id" v-bind:value="projectType.id">{{ projectType.name }}</option>
+                </select>
+                
+                <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Project information
+            </label>
+            <textarea row=4 class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"></textarea>
+        </div>
+        
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                City/Area
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text">
+        </div>
+
+        <div class="flex flex-col mb-4">
+            <h3 class="block uppercase tracking-wide">Positions needed</h3>
+            <cca-department></cca-department>
+        </div>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">Show my production company name publicly?</label>
+            <div>
+                <input name="public_production" type="radio" selected> Yes
+                <input name="public_production" type="radio"> No
+                <!-- TODO: need to defer to producer uri instead of admin -->
+                <a href="/">See site list</a>
+            </div>
+        </div>
+
+        <p class="mb-4">Only roles that accept video auditions and are paid, or audio auditions can be posted on ther sites.</p>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Sites to post on
+            </label>
+
+            <input type="checkbox"> Check All
+            <input type="checkbox"> Yourcasting Test Site
+
+            <div v-if="sites">
+                <input v-for="site in sites" :key="site.id" type="checkbox"> {{ site.name }}
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Will travel/lodging expenses be paid for out-of-area talent?
+            </label>
+
+            <input name="paid_travel" type="radio"> Yes
+            <input name="paid_travel" type="radio"> No
+        </div>
+
+        <div class="mb-4">
+            <button class="bg-blue hover:bg-blue-dark text-white font-bold w-24 py-2 px-4 rounded-full float-right w-64">Save</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Department from './forms/Department.vue'
+
+    export default {
+        components: {
+            'cca-department': Department
+        },
+        data(){
+            return {
+                projectTypes: null,
+                sites: null
+            }
+        }
+    }
+</script>
