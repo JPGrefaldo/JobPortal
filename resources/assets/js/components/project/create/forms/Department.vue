3004d630e4edd37ef48297291283d0e5b5292d89
Rafa Rafael <rafa@hjugroup.io>
2019-03-12 21:53:37 +0800

    Refactor and redesign the ui and add method for handling filter to show by category

--- resources/assets/js/components/project/create/forms/Department.vue
+++ resources/assets/js/components/project/create/forms/Department.vue
@@ -1,6 +1,14 @@
 <template>
     <div v-if="departments">
-        <input v-model="department.name" type="button" v-for="department in departments" :key="department.id">
+        <ul class="flex list-reset rounded w-auto">
+            <li>
+                <input  class="hover:text-white hover:bg-blue text-blue border-r border-grey-light px-3 py-2"
+                        v-model="department.name" type="button" 
+                        v-for="department in departments" :key="department.id" 
+                        @click="showByCategory(department.id)">
+            </li>
+            
+        </ul>
     </div>
 </template>
 
@@ -8,14 +16,32 @@
     import { mapGetters } from 'vuex'
 
     export default {
+        data(){
+            return {
+                allPositions: []
+            }
+        },
+
         mounted(){
             this.$store.dispatch('crew/fetchDepartments')
         },
 
         computed: {
             ...mapGetters({
-                departments: 'crew/departments'
+                departments: 'crew/departments',
+                positions: 'crew/positions'
             })
+        },
+
+        methods: {
+            showByCategory(id){
+                if (this.allPositions.length === 0){
+                    this.allPositions = this.positions
+                }
+
+                let positions = this.allPositions.filter(o => o.department_id == id)
+                this.$store.commit('crew/POSITIONS', positions)
+            }
         }
     }
 </script>
