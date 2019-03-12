0663270cf83db08d4e28294a801446bfd70e2d57
Rafa Rafael <rafa@hjugroup.io>
2019-03-12 21:51:20 +0800

    Add store method and some validation

--- app/Http/Controllers/API/Producer/ProjectsController.php
+++ app/Http/Controllers/API/Producer/ProjectsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\API\Producer;
 
+use App\Actions\Producer\Project\CreateProject;
+use App\Actions\Producer\Project\CreateProjectJob;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
 use App\Models\ProjectType;
+use App\Models\Site;
+use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
@@ -23,5 +27,25 @@ class ProjectsController extends Controller
         return response()->json([
             'projectType' => $types
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $user = auth()->user()->id;
+        $site_ids = $request->sites;
+
+        if (count($site_ids) === 1 && $site_ids[0] === 'all'){
+            $site_ids = Site::all()->pluck('id');
+        }
+
+        foreach ($site_ids as $site_id){
+            $project = app(CreateProject::class)->execute($user, $site_id, $request);
+        }
+
+        if (isset($project->id) && count($request->project_job) > 0){
+            foreach($request->project_job as $job){
+                app(CreateProjectJob::class)->execute($job, $project, $request);
+            }
+        }
     }
 }
