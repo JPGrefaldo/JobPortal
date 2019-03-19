528d0f89e8d6db6c3ed6c1e29329af5e540fd06b
Mike Castro <mikekevin.castro@hjugroup.io>
2019-03-19 23:00:02 +0800

    reorder test methods

--- tests/Unit/Actions/Crew/EditCrewReelTest.php
+++ tests/Unit/Actions/Crew/EditCrewReelTest.php
@@ -114,46 +114,6 @@ class EditCrewReelTest extends TestCase
 
     /**
      * @test
-     * @covers \App\Actions\Crew\EditCrewReel::execute
-     */
-    public function reel_file_can_be_updated_to_reel_file()
-    {
-        // given
-        Storage::fake('s3');
-
-        $user = $this->createUser();
-        $createData = $this->getCreateData([
-            'reel' => UploadedFile::fake()->create('old-reel.mp4'),
-        ]);
-
-        app(StoreCrew::class)->execute($user, $createData);
-
-        $crew = $user->crew;
-        $data = $this->getUpdateData([
-            'reel' => UploadedFile::fake()->create('new-reel.mp4'),
-        ]);
-        $nonExpectedPath = $crew->user->hash_id . '/reels/'. $createData['reel']->hashName();
-
-        // when
-        app(EditCrewReel::class)->execute($crew, $data);
-
-        // then
-        Storage::disk('s3')->assertMissing($nonExpectedPath);
-
-        $expectedPath = $crew->user->hash_id . '/reels/'. $data['reel']->hashName();
-
-        Storage::disk('s3')->assertExists($expectedPath);
-
-        $this->assertDatabaseHas('crew_reels', [
-            'crew_id'          => $user->crew->id,
-            'path'             => $expectedPath,
-            'general'          => true,
-            'crew_position_id' => null,
-        ]);
-    }
-
-    /**
-     * @test
      * @covers \App\Actions\Crew\EditCrew::execute
      */
     public function reel_link_can_be_updated_to_reel_file()
@@ -216,6 +176,46 @@ class EditCrewReelTest extends TestCase
         $this->assertDatabaseHas('crew_reels', [
             'crew_id'          => $user->crew->id,
             'path'             => 'https://www.youtube.com/embed/WI5AF1DCQlc',
+            'general'          => true,
+            'crew_position_id' => null,
+        ]);
+    }
+
+    /**
+     * @test
+     * @covers \App\Actions\Crew\EditCrewReel::execute
+     */
+    public function reel_file_can_be_updated_to_reel_file()
+    {
+        // given
+        Storage::fake('s3');
+
+        $user = $this->createUser();
+        $createData = $this->getCreateData([
+            'reel' => UploadedFile::fake()->create('old-reel.mp4'),
+        ]);
+
+        app(StoreCrew::class)->execute($user, $createData);
+
+        $crew = $user->crew;
+        $data = $this->getUpdateData([
+            'reel' => UploadedFile::fake()->create('new-reel.mp4'),
+        ]);
+        $nonExpectedPath = $crew->user->hash_id . '/reels/'. $createData['reel']->hashName();
+
+        // when
+        app(EditCrewReel::class)->execute($crew, $data);
+
+        // then
+        Storage::disk('s3')->assertMissing($nonExpectedPath);
+
+        $expectedPath = $crew->user->hash_id . '/reels/'. $data['reel']->hashName();
+
+        Storage::disk('s3')->assertExists($expectedPath);
+
+        $this->assertDatabaseHas('crew_reels', [
+            'crew_id'          => $user->crew->id,
+            'path'             => $expectedPath,
             'general'          => true,
             'crew_position_id' => null,
         ]);
