38466be319fdcbdbcb153918b74d499f7b790913
Rafa Rafael <rafa@hjugroup.io>
2019-03-25 12:45:32 +0800

    Extract the reusable form for ProjectCreate and ProjectEdit

---
+++ resources/assets/js/components/project/ProjectEdit.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="md:w-3/4 float-left">
+       <project-form></project-form>
+    </main>
+</template>
+
+<script>
+    import ProjectForm from './ProjectForm.vue'
+
+    export default {
+        props: {
+            project: {
+                type: Object,
+                required: true
+            }
+        },
+
+        components: {
+            'project-form': ProjectForm
+        },
+
+        mounted() {
+            this.$store.commit('project/PROJECT', this.project)
+        }
+    }
+</script>
