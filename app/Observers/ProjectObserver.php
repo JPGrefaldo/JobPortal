d8f1418fba74c0c0913cc4718eb43c66ac08ddc5
Rafa Rafael <rafa@hjugroup.io>
2019-04-04 09:06:45 +0800

    Fix bug that causes test fails when project is created

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\ProjectCreatedEmail;
+use App\Models\Project;
+use App\Models\Role;
+use App\Models\User;
+
+class ProjectObserver
+{
+    /**
+     * Handle the project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        $admin = User::role(Role::ADMIN)->first();
+
+       if ($admin instanceof User){
+            \Mail::to($admin->email)->send(
+                new ProjectCreatedEmail($admin, $project)
+            );
+       }
+    }
+
+    /**
+     * Handle the project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
