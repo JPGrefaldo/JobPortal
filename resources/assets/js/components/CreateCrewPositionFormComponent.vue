7f26769888d59197e410d376a5afa05016f1af3f
Rafa Rafael <rafa@hjugroup.io>
2019-04-05 16:47:00 +0800

    Format the vue components

--- resources/assets/js/components/CreateCrewPositionFormComponent.vue
+++ resources/assets/js/components/CreateCrewPositionFormComponent.vue
@@ -8,9 +8,11 @@
                     </div>
                     <textarea
                         name="details"
-                        cols="30" rows="10"
+                        cols="30"
+                        rows="10"
                         class="w-full"
-                        v-model="form.details"></textarea>
+                        v-model="form.details"
+                    ></textarea>
                     <p class="w-1/3 text-red text-xs italic">
                         {{ form.errors.get('details') }}
                     </p>
@@ -25,19 +27,20 @@
                         cols="30"
                         rows="10"
                         class="w-full"
-                        v-model="form.union_description"></textarea>
+                        v-model="form.union_description"
+                    ></textarea>
                     <p class="w-1/3 text-red text-xs italic">
                         {{ form.errors.get('union_description') }}
                     </p>
                 </div>
-
             </div>
 
             <button
                 type="submit"
                 class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 mt-4 rounded"
                 @click.prevent="createCrewPosition"
-                :disabled="form.busy">
+                :disabled="form.busy"
+            >
                 Apply
             </button>
         </form>
@@ -45,32 +48,30 @@
 </template>
 
 <script type="text/javascript">
-    import Form from '../form.js';
+import Form from '../form.js';
 
-    export default {
-        name: "CreateCrewPositionFormComponent",
-        props: ['url'],
-        data() {
-            return {
-                form: new Form({
-                    details: '',
-                    union_description: '',
+export default {
+    name: 'CreateCrewPositionFormComponent',
+    props: ['url'],
+    data() {
+        return {
+            form: new Form({
+                details: '',
+                union_description: '',
+            }),
+        };
+    },
+    methods: {
+        createCrewPosition() {
+            this.form
+                .post(this.url, this.form)
+                .then(response => {
+                    window.location = '../';
                 })
-            }
+                .catch(response => {
+                    this.form.errors.record(response.errors);
+                });
         },
-        methods: {
-            createCrewPosition() {
-                this
-                    .form
-                    .post(this.url, this.form)
-                    .then(response => {
-                        window.location = '../';
-                    })
-                    .catch(response => {
-                        this.form.errors.record(response.errors);
-                    });
-            },
-        }
-    }
+    },
+};
 </script>
-
--- resources/assets/js/components/MobileMenuComponent.vue
+++ resources/assets/js/components/MobileMenuComponent.vue
@@ -1,24 +1,27 @@
 <template>
     <li class="py-2 px-4">
-        <a v-bind:href="link" class="block text-blue-dark hover:text-green" @click="follow($event)">{{ text }}</a>
+        <a
+            v-bind:href="link"
+            class="block text-blue-dark hover:text-green"
+            @click="follow($event)"
+            >{{ text }}</a
+        >
     </li>
 </template>
 
 <script type="text/javascript">
-    export default {
-        name: "MobileMenuComponent",
-        props: {
-            link: String,
-            text: String,
+export default {
+    name: 'MobileMenuComponent',
+    props: {
+        link: String,
+        text: String,
+    },
+    methods: {
+        follow: function(event) {
+            window.location = event.target.href;
         },
-        methods: {
-            follow: function(event) {
-                window.location = event.target.href;
-            }
-        }
-    };
+    },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
