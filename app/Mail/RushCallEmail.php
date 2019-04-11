b9d477b0409cfd18e75f7221bac73025a0a2edb7
Rafa Rafael <rafa@hjugroup.io>
2019-04-11 13:31:35 +0800

    Create a RushCallEmail Mailable and rush-call markdown template  and SendingEmail action class. (needs approval if this was ok) for sending email.

---
+++ app/Mail/RushCallEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RushCallEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $crew;
+
+    public $projectJob;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($crew, $projectJob)
+    {
+        $this->crew         = $crew;
+        $this->projectJob   = $projectJob;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.rush-call');
+    }
+}
--- resources/views/emails/rush-call.blade.php
+++ resources/views/emails/rush-call.blade.php
@@ -1,12 +1,31 @@
 @component('mail::message')
-# Introduction
+# Hi {{ $crew->nickname }},
 
-The body of your message.
-
-@component('mail::button', ['url' => ''])
-Button Text
+Add new experience to your resume. Apply now!
+<hr>
+@component('mail::panel')
+## [{{ $projectJob->position_name }}]({{ route('crew.project.vacancy', $projectJob->id) }})
+@if (isset($projectJob->pay_rate) && $projectJob->pay_rate != 0)
+Salary: $ {{ $projectJob->pay_rate }}.00 / {{ $projectJob->pay_type }}
+@else
+Pay Type: {{ $projectJob->pay_type }}
+@endif
+<br>
+Persons Needed: {{ $projectJob->persons_needed }}
+<br>
+Dates Needed:
+@if (is_array($projectJob->dates_needed))
+@foreach ($projectJob->dates_needed as $date)
++ {{ $date }}
+@endforeach
+@else
+{{ $projectJob->dates_needed }}
+@endif 
+@endcomponent
+@component('mail::button', ['url' => route('admin.projects')])
+Apply
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+<hr>
+This e-mail has been automatically generated. If you wish to contact us, please click on [{{ config('app.name') }}]({{ route('home') }})
 @endcomponent
