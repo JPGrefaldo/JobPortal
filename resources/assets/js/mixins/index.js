c253bfdd0ae40e53e8dd5157b2c44ae8dc37a950
Rafa Rafael <rafa@hjugroup.io>
2019-04-16 16:49:20 +0800

    Add more format options for dates

--- resources/assets/js/mixins/index.js
+++ resources/assets/js/mixins/index.js
@@ -1,6 +1,7 @@
 import alert from './alert.js';
 import color from './color.js';
-import messageFlag from './message-flag.js';
 import format from './format.js';
+import messageFlag from './message-flag.js';
+import modals from './modals';
 
-export { alert, color, messageFlag, format };
+export { alert, color, format, messageFlag, modals };
