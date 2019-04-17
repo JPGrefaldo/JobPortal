00777098ab72bbb18764acd166f570569b0d5c75
Rafa Rafael <rafa@hjugroup.io>
2019-04-17 10:09:17 +0800

    Add and used vue.js example modal for adding role

---
+++ resources/assets/js/components/_partials/Modal.vue
@@ -0,0 +1,94 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container overflow-auto h-full">
+
+                <div class="modal-header">
+                    <slot name="header">
+                        <span class="float-right" @click="$emit('close')">
+                            <svg class="fill-current h-6 w-6 text-red" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                        </span>
+                    </slot>
+                </div>
+
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        display: table;
+        transition: opacity .3s ease;
+    }
+
+    .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .modal-container {
+        width: 75%;
+        margin: 0px auto;
+        padding: 20px 30px;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+        transition: all .3s ease;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+
+    .modal-header h3 {
+        margin-top: 0;
+        color: #42b983;
+    }
+
+    .modal-body {
+        margin: 20px 0;
+    }
+
+    .modal-default-button {
+        float: right;
+    }
+
+    /*
+    * The following styles are auto-applied to elements with
+    * transition="modal" when their visibility is toggled
+    * by Vue.js.
+    *
+    * You can easily play with the modal transition by editing
+    * these styles.
+    */
+
+    .modal-enter {
+        opacity: 0;
+    }
+
+    .modal-leave-active {
+        opacity: 0;
+    }
+
+    .modal-enter .modal-container,
+    .modal-leave-active .modal-container {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
+</style>
---
+++ resources/assets/js/components/project/ProjectJobCreateModal.vue
@@ -0,0 +1,25 @@
+<template>
+   <modal @close="close">
+        <div slot="body">
+            <project-job-create></project-job-create>
+        </div>
+    </modal>
+</template>
+
+<script>
+    import ProjectJobCreate from './ProjectJobCreate.vue'
+    import Modal from '../_partials/Modal.vue'
+
+    export default {
+        components: {
+            'modal' : Modal,
+            'project-job-create': ProjectJobCreate
+        },
+
+        methods: {
+            close: function() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
