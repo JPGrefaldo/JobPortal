16764653dd5489d6da17e18d7059f46e612579c7
Marc-Pasalo <marc@hjugroup.io>
2019-04-22 19:41:57 +0800

    added a link to the thread subject

--- resources/assets/js/components/messenger/MessengerDashboard.vue
+++ resources/assets/js/components/messenger/MessengerDashboard.vue
@@ -6,7 +6,7 @@
             <div
                 class="w-4/5 text-md border-black border-b font-bold flex justify-center items-center"
             >
-                <div v-if="project.title">{{ project.title }}: {{ thread.subject }}</div>
+                <div v-if="project.title">{{ project.title }}: <a :href="'/crews/1'">{{ thread.subject }}</a></div>
                 <div v-else>No Project Selected</div>
             </div>
         </div>
