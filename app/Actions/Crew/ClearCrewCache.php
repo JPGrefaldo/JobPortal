b7b4b069d28f0d1a222fa595c30ff99d0a7a5cb0
Bryan Heath <35047806+cca-bheath@users.noreply.github.com>
2019-04-22 19:19:37 -0700

    Stub cache clearing actions (#238)

---
+++ app/Actions/Crew/ClearCrewCache.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions\Crew;
+
+
+use App\Models\Crew;
+
+class ClearCrewCache
+{
+    /**
+     * @param Crew $crew
+     */
+    public function execute(Crew $crew): void
+    {
+
+    }
+}
