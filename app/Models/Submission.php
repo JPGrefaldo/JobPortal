4c8e40209900c975919241b9f3f58f811fbd0239
Rafa Rafael <rafa@hjugroup.io>
2019-04-23 08:33:13 +0800

    Setup relationship between submissions and project job

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function job()
+    {
+        return $this->belongsTo(Submission::class);
+    }
 }
