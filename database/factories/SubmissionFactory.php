8246234406e871251e5d78114b3f42143fe18445
Rafa Rafael <rafa@hjugroup.io>
2019-04-24 14:23:14 +0800

    Add submission factory and migration

---
+++ database/factories/SubmissionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\ProjectJob;
+use App\Models\Submission;
+use App\Models\User;
+
+$factory->define(Submission::class, function() {
+    return [
+        'crew_id'           => factory(User::class),
+        'project_job_id'    => factory(ProjectJob::class)
+    ];
+});
