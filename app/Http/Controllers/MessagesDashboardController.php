43fa64651c06a11bddb8f412ef26263805dc431d
Marc Paul Pasalo <marc@hjugroup.io>
2019-04-25 16:11:53 +0800

    undone changes to controller

--- app/Http/Controllers/MessagesDashboardController.php
+++ app/Http/Controllers/MessagesDashboardController.php
@@ -6,7 +6,7 @@ class MessagesDashboardController extends Controller
 {
     public function index()
     {
-        $user = auth()->user()->load('crew');
+        $user = auth()->user();
         $roles = $user->roles->pluck('name');
         
         return view('messages-dashboard', compact('user', 'roles'));
