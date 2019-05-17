042578fc0a828169e380770a7b1f498f009327bd
Rafa Rafael <rafa@hjugroup.io>
2019-05-17 12:24:34 +0800

    Refactor to not show the messages where the user is not participated in the current project

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,28 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Actions\Messenger\CreateMessage;
 use App\Http\Resources\MessageResource;
+use App\Models\ProjectThread;
 use App\Models\Role;
-use Cmgmyr\Messenger\Models\Thread;
+use App\Models\Thread;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MessagesController extends Controller
 {
+    protected $userIsParticipant = false;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index(Thread $thread)
-    {
-        $user = auth()->user();
-
-        if (! $thread->hasParticipant($user->id)) {
-            return abort(403);
-        }
-
+    {   
+        $authUser = auth()->user();
         $messages = $thread->messages()->with('user')->where('flagged_at', null)->get();
 
-        return MessageResource::collection($messages);
+        $messages->map(function($message) use($authUser) {
+            if ($message->user->id === $authUser->id) {
+                $this->userIsParticipant = true;
+            } 
+        });
+
+        if ($this->userIsParticipant) {
+            return MessageResource::collection($messages);
+        }
+
+        return response()->json([], Response::HTTP_FORBIDDEN);
     }
 
     /**
@@ -36,18 +44,37 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
-        $sender = auth()->user();
-        
-        if ($sender->hasRole(Role::CREW)) {
-            return response()->json([
-                'message' => 'You are not allowed to message the producer',
-            ]);
-        }
+        $user = auth()->user();
 
-        $thread = Thread::findOrFail($request->thread);
+        $thread = Thread::find($request->thread);
+
+        if (empty($thread)) {
+            if ($user->hasRole(Role::CREW)) {
+                return response()->json([
+                    'message' => 'You are not allowed to message the producer',
+                ]);
+            }
+
+            $thread = $user->project()->threads()->save([
+                'subject' => $request->subject
+            ]);
+
+            ProjectThread::create([
+                'project_id' => $user->project->id,
+                'thread_id'  => $thread->id
+            ]);
+
+            //threads table
+            $thread->create([
+                'subject' => $request->subject
+            ]);
+
+            $thread->addParticipant($request->participant);
+        }
+        
         $body = $request->message;
         
-        $message = app(CreateMessage::class)->execute($thread->id, $sender->id, $body);
+        $message = app(CreateMessage::class)->execute($thread, $user, $body);
         
         return response()->json(compact('message'));
     }
