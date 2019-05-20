d2e405d7382363808ca39570f9192465072cf7e1
Marc Paul Pasalo <marc@hjugroup.io>
2019-05-20 09:08:47 +0800

    ungrouped other routes

--- routes/api.php
+++ routes/api.php
@@ -80,107 +80,101 @@ Route::middleware('auth:api')->group(function () {
     ])->name('crew.threads.index');
 
     Route::prefix('producer')->middleware('role:Producer')->group(function () {
-        Route::prefix('projects')->group(function () {
-            Route::get('submissions/{job}', [
-                ProjectJobSubmissionController::class,
-                'index',
-            ])->name('project.job.submissions.index');
+        
+        Route::get('/projects/submissions/{job}', [
+            ProjectJobSubmissionController::class,
+            'index',
+        ])->name('project.job.submissions.index');
 
-            Route::get('/', [
-                ProjectController::class,
-                'index',
-            ])->name('producer.projects.index');
+        Route::get('/projects', [
+            ProjectController::class,
+            'index',
+        ])->name('producer.projects.index');
 
-            Route::post('/', [
-                ProjectController::class,
-                'store',
-            ])->name('producer.project.store');
+        Route::post('/projects', [
+            ProjectController::class,
+            'store',
+        ])->name('producer.project.store');
 
-            Route::put('/{project}', [
-                ProjectController::class,
-                'update',
-            ])->name('producer.projects.update');
+        Route::put('/projects/{project}', [
+            ProjectController::class,
+            'update',
+        ])->name('producer.projects.update');
 
-            Route::get('/{project}/threads', [
-                ThreadController::class,
-                'index',
-            ])->name('producer.threads.index');
+        Route::get('/projects/{project}/threads', [
+            ThreadController::class,
+            'index',
+        ])->name('producer.threads.index');
 
-            Route::get('/approved', [
-                ProjectController::class,
-                'approved',
-            ])->name('producer.projects.approved');
+        Route::get('/projects/approved', [
+            ProjectController::class,
+            'approved',
+        ])->name('producer.projects.approved');
 
-            Route::prefix('jobs')->group(function () {
-                Route::get('/', [
-                    ProjectJobController::class,
-                    'index',
-                ])->name('producer.project.jobs');
+        Route::get('/projects/jobs', [
+            ProjectJobController::class,
+            'index',
+        ])->name('producer.project.jobs');
 
-                Route::post('/', [
-                    ProjectJobController::class,
-                    'store',
-                ])->name('producer.project.jobs.store');
+        Route::post('/projects/jobs', [
+            ProjectJobController::class,
+            'store',
+        ])->name('producer.project.jobs.store');
 
-                Route::put('/{projectJob}', [
-                    ProjectJobController::class,
-                    'update',
-                ])->name('producer.project.jobs.update');
+        Route::put('/projects/jobs/{projectJob}', [
+            ProjectJobController::class,
+            'update',
+        ])->name('producer.project.jobs.update');
 
-                Route::delete('/{projectJob}', [
-                    ProjectJobController::class,
-                    'destroy',
-                ])->name('producer.project.jobs.destroy');
+        Route::delete('/projects/jobs/{projectJob}', [
+            ProjectJobController::class,
+            'destroy',
+        ])->name('producer.project.jobs.destroy');
 
-                Route::get('/{projectJob}/submissions/all-approved', [
-                    SubmissionController::class,
-                    'fetchByApprovedDate',
-                ])->name('fetch.submissions.by.approved');
-            });
+        Route::get('/projects/jobs/{projectJob}/submissions/all-approved', [
+            SubmissionController::class,
+            'fetchByApprovedDate',
+        ])->name('fetch.submissions.by.approved');
 
-            Route::post('/submissions/{submission}/approve', [
-                SubmissionController::class,
-                'approve',
-            ])->name('producer.projects.approve.submissions');
+        Route::post('/projects/submissions/{submission}/approve', [
+            SubmissionController::class,
+            'approve',
+        ])->name('producer.projects.approve.submissions');
 
-            Route::post('swap/submissions/{submissionToReject}/{submissionToApprove}', [
-                SubmissionController::class,
-                'swap',
-            ])->name('producer.projects.swap.submissions');
-            Route::post('/submissions/{submission}/reject', [
-                SubmissionController::class,
-                'reject',
-            ])->name('producer.projects.submissions.reject');
+        Route::post('/projects/swap/submissions/{submissionToReject}/{submissionToApprove}', [
+            SubmissionController::class,
+            'swap',
+        ])->name('producer.projects.swap.submissions');
+        Route::post('/projects/submissions/{submission}/reject', [
+            SubmissionController::class,
+            'reject',
+        ])->name('producer.projects.submissions.reject');
 
-            Route::post('/submissions/{submission}/restore', [
-                SubmissionController::class,
-                'restore',
-            ])->name('producer.projects.submissions.restore');
+        Route::post('/projects/submissions/{submission}/restore', [
+            SubmissionController::class,
+            'restore',
+        ])->name('producer.projects.submissions.restore');
 
-            Route::get('/pending', [
-                ProjectController::class,
-                'pending',
-            ])->name('producer.projects.pending');
+        Route::get('/projects/pending', [
+            ProjectController::class,
+            'pending',
+        ])->name('producer.projects.pending');
 
-            Route::get('/type', [
-                ProjectTypes::class,
-                'index',
-            ])->name('producer.project.type');
-        });
+        Route::get('/projects/type', [
+            ProjectTypes::class,
+            'index',
+        ])->name('producer.project.type');
+        
 
-        Route::prefix('messages')->group(function () {
-            Route::prefix('templates')->group(function () {
-                Route::get('/', [
-                    MessageTemplateController::class,
-                    'index',
-                ])->name('producer.messages.templates');
+        Route::get('/messages/templates', [
+            MessageTemplateController::class,
+            'index',
+        ])->name('producer.messages.templates');
 
-                Route::post('/', [
-                    MessageTemplateController::class,
-                    'store',
-                ])->name('producer.messages.templates');
-            });
-        });
+        Route::post('/messages/templates', [
+            MessageTemplateController::class,
+            'store',
+        ])->name('producer.messages.templates');
 
         Route::get('/pending', [
             ProjectController::class,
