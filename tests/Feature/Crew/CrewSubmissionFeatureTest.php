0157dcd542c7c0e92a17678c4e9193d82cd158fd
Mike Castro <mikekevin.castro@hjugroup.io>
2019-05-22 10:04:23 +0800

    move tests cleanup

--- tests/Feature/Crew/CrewSubmissionFeatureTest.php
+++ tests/Feature/Crew/CrewSubmissionFeatureTest.php
@@ -3,19 +3,14 @@
 namespace Tests\Feature\Crews;
 
 use App\Models\CrewResume;
-use App\Models\Position;
 use App\Models\ProjectJob;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Http\UploadedFile;
 use Tests\Support\SeedDatabaseAfterRefresh;
 use Tests\TestCase;
 
 class CrewSubmissionFeatureTest extends TestCase
 {
-    use RefreshDatabase,
-        SeedDatabaseAfterRefresh,
-        WithFaker;
+    use RefreshDatabase, SeedDatabaseAfterRefresh;
 
     /**
      * @test
@@ -66,74 +61,6 @@ class CrewSubmissionFeatureTest extends TestCase
             'crew_id'              => $crew->id,
             'project_id'           => $job->project_id,
             'project_job_id'       => $job->project_id,
-        ]);
-    }
-
-    /**
-     * @test
-     * @covers \App\Http\Controllers\Crew\CrewProfileController::store
-     */
-    public function create_position_requires_gear()
-    {
-        $crew     = $this->createCrew();
-        $position = factory(Position::class)->create([
-            'has_gear' => 1,
-        ]);
-
-        $data = [
-            'position_id'       => $position->id,
-            'bio'               => 'This is the bio',
-            'resume'            => UploadedFile::fake()->create('resume.doc'),
-            'union_description' => 'Some union description',
-            'reel_link'         => 'http://www.youtube.com/embed/G8S81CEBdNs',
-        ];
-
-        $this->actingAs($crew)
-            ->postJson(route('crew-position.store', $position), $data)
-            ->assertJsonValidationErrors('gear');
-
-        $this->assertDatabaseMissing('crew_position', [
-            'crew_id'           => $crew->id,
-            'details'           => $data['bio'],
-            'union_description' => $data['union_description'],
-        ]);
-
-        $this->assertDatabaseMissing('crew_reels', [
-            'crew_id' => $crew->id,
-            'path'    => $data['reel_link'],
-        ]);
-    }
-
-    /**
-     * @test
-     * @covers \App\Http\Controllers\Crew\CrewProfileController::store
-     */
-    public function create_position_dont_require_gear()
-    {
-        $crew     = $this->createCrew();
-        $position = factory(Position::class)->create();
-
-        $data = [
-            'position_id'       => $position->id,
-            'bio'               => 'This is the bio',
-            'resume'            => UploadedFile::fake()->create('resume.doc'),
-            'union_description' => 'Some union description',
-            'reel_link'         => "http://www.youtube.com/embed/G8S81CEBdNs",
-        ];
-
-        $this->actingAs($crew)
-            ->postJson(route('crew-position.store', $position), $data)
-            ->assertSessionDoesntHaveErrors();
-
-        $this->assertDatabaseHas('crew_position', [
-            'crew_id'           => $crew->id,
-            'details'           => $data['bio'],
-            'union_description' => $data['union_description'],
-        ]);
-
-        $this->assertDatabaseMissing('crew_reels', [
-            'crew_id' => $crew->id,
-            'path'    => $data['reel_link'],
         ]);
     }
 }
