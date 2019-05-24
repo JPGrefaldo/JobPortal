9e0b0783ffb98da055e9227392f5e07a71a4d6a5
Rafa Rafael <rafa@hjugroup.io>
2019-05-24 08:07:04 +0800

    Restore broken implementation

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -1,17 +1,23 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Actions\Messenger\StoreMessage;
+use App\Actions\Messenger\StoreParticipants;
+use App\Actions\Messenger\StoreThread;
+use App\Actions\Messenger\UpdateParticipants;
+use App\Actions\Producer\StoreMessageCrew;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\MessageResource;
+use App\Models\Project;
 use App\Models\Role;
 use App\Models\Thread;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\Producer\Message\StoreMessageCrewRequest;
 
 class MessageController extends Controller
 {
-    protected $userIsParticipant = false;
     /**
      * Display a listing of the resource.
      *
@@ -20,44 +26,78 @@ class MessageController extends Controller
     public function index(Thread $thread)
     {
         $user = auth()->user();
-        $messages = $thread->messages()->with('user')->where('flagged_at', null)->get();
-
+    
         if (! $thread->hasParticipant($user->id)) {
             return response()->json([], Response::HTTP_FORBIDDEN);
         }
 
+        $messages = $thread->messages()->with('user')->where('flagged_at', null)->get();
         return MessageResource::collection($messages);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a new project thread and its message and participant(recipient).
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Project $project, Request $request)
+    {
+        $user = auth()->user();
+      
+        if ($user->hasRole(Role::CREW)) {
+            return response()->json([
+                'message' => 'You are not allowed to initiate a conversation with any producer.',
+            ]);
+        }
+
+        $thread  = app(StoreThread::class)->execute($project, $request->subject);
+        $message = app(StoreMessage::class)->execute($thread, $user, $request->message);
+        
+        app(StoreParticipants::class)->execute($thread, $user, $request->recipient);
+
+        return response()->json(
+            compact('message'), 
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Store a new project, thread and its message and participants(recepients).
+     *
+     * @param  \App\Models\Project $project
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function storeCrew(Project $project, StoreMessageCrewRequest $request)
     {
         $user = auth()->user();
 
-        $thread = Thread::find($request->thread);
-
-        if (empty($thread)) {
-            if ($user->hasRole(Role::CREW)) {
-                return response()->json([
-                    'message' => 'You are not allowed to initiate a conversation with any producer.',
-                ]);
-            }
-
-            $thread = app(CreateThread::class)->execute($user, $request);
-        }
-
-        $message = app(StoreMessage::class)->execute($thread, $user, $request->message);
+        app(StoreMessageCrew::class)->execute($project, $user, $request);
 
         return response()->json(
-            [
-                'message' => compact('message')
-            ],
+            ['message' => "Successfully messaged crews."], 
             Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Update thread participants and store message replies
+     *
+     * @param \App\Models\Thread $thread
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Thread $thread, Request $request)
+    {
+        $user    = auth()->user();
+        $message = app(StoreMessage::class)->execute($thread, $user, $request->message);
+
+        app(UpdateParticipants::class)->execute($thread, $user, $request->recipient);
+
+        return response()->json(
+            compact('message'), 
+            Response::HTTP_OK
         );
     }
 }
