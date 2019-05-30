cf3852dcd1d564985b0aa73de3791b80835cbf47
JPGrefaldo <jp.grefaldo@hjugroup.io>
2019-05-30 14:26:12 +0800

    update current project page, fix duplicate main tag

--- resources/assets/js/components/crew/CrewProjectList.vue
+++ resources/assets/js/components/crew/CrewProjectList.vue
@@ -1,7 +1,7 @@
 <template>
-<main class="md:w-3/4 float-left">
+<div class="md:w-3/4 float-left">
      <project-card v-for="project in projects" :key="project.id" :project="project"/>
-</main>
+</div>
 </template>
 
 <script>
--- resources/views/layouts/page-2-col.blade.php
+++ resources/views/layouts/page-2-col.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-    <main class="max-w-screen-xl flex justify-center items-center py-md md:py-lg px-3">
+    <div class="max-w-screen-xl flex justify-center items-center py-md md:py-lg px-3">
         <div class="xl:w-1/2">
             @yield('page-content')
         </div>
-    </main>
+    </div>
 
 @endsection
