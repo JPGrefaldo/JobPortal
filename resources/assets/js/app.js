41b62585ea35ab53111c4dfb9a5749b750e3a4bb
Rafa Rafael <rafa@hjugroup.io>
2019-06-03 11:22:05 +0800

    Register vue component and add blade view to load

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -55,6 +55,7 @@ Vue.component('cca-producer-projects-edit', require('./components/project/Projec
 Vue.component('cca-producer-my-projects', require('./components/producer/Projects').default);
 Vue.component('cca-project-job-submissions', require('./components/project/ProjectJobSubmissions').default);
 Vue.component('cca-crew-current-projects', require('./components/crew/CrewProjectList').default);
+Vue.component('cca-crew-projects', require('./components/crew/CrewProjectDashboard').default);
 Vue.component('file-pond', require('./components/FilePondComponent').default);
 
 new Vue({
