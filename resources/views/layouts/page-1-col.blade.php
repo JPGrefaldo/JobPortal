3dfb64be94bde62d3fe7993fff786a531bafae58
Marc Paul Pasalo <marc@hjugroup.io>
2019-06-04 21:15:14 +0800

    visual improvements for projects page

--- resources/views/layouts/page-1-col.blade.php
+++ resources/views/layouts/page-1-col.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-    <main class="max-w-screen-xl flex justify-center items-center py-md md:py-lg px-3">
+    <div class="max-w-screen-xl flex justify-center items-center py-md md:py-lg px-3">
             @yield('page-content')
-    </main>
+    </div>
 
 @endsection
