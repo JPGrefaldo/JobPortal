ce6862c9c69d22c298517f722dbe28af3ad3e700
Rafa Rafael <rafa@hjugroup.io>
2019-06-05 13:48:09 +0800

    Rename model to singular

--- app/Models/CrewIgnoredJob.php
+++ app/Models/CrewIgnoredJob.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CrewIgnoredJobs extends Model
+class CrewIgnoredJob extends Model
 {
     //
 }
--- database/factories/CrewIgnoredJobsFactory.php
+++ database/factories/CrewIgnoredJobsFactory.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Models\Crew;
-use App\Models\CrewIgnoredJobs;
+use App\Models\CrewIgnoredJob;
 use App\Models\ProjectJob;
 use Faker\Generator as Faker;
 
-$factory->define(CrewIgnoredJobs::class, function(Faker $faker) {
+$factory->define(CrewIgnoredJob::class, function(Faker $faker) {
     return [
         'crew_id'        => factory(Crew::class),
         'project_job_id' => factory(ProjectJob::class)
