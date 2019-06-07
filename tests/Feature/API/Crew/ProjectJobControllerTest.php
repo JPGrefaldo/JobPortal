cb70ee0432e25d296e77038c0393231ab7404ce2
Rafa Rafael <rafa@hjugroup.io>
2019-06-07 12:54:54 +0800

    Add test case for crew's submissions and fcs

--- tests/Feature/API/Crew/ProjectJobControllerTest.php
+++ tests/Feature/API/Crew/ProjectJobControllerTest.php
@@ -11,6 +11,7 @@ use App\Models\Submission;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Support\SeedDatabaseAfterRefresh;
 use Tests\TestCase;
+use Carbon\Carbon;
 
 class ProjectJobControllerTest extends TestCase
 {
@@ -30,8 +31,6 @@ class ProjectJobControllerTest extends TestCase
             ->postJson(route('crew.ignore.jobs', $job))
             ->assertSee('Successfully ignored the job')
             ->assertSuccessful();
-
-        $this->assertCount(1, CrewIgnoredJob::all());
     }
 
     /**
@@ -42,6 +41,8 @@ class ProjectJobControllerTest extends TestCase
     {
         $crew   = $this->createCrew();
         $job    = $this->seedIgnoredJob($crew);
+
+        $this->assertCount(1, CrewIgnoredJob::all());
 
         $this->actingAs($crew, 'api')
             ->postJson(route('crew.unignore.jobs', $job))
@@ -56,19 +57,76 @@ class ProjectJobControllerTest extends TestCase
      * @test
      * @covers App\Http\Controllers\API\Crew/ProjectJobController::ignored
      */
-    public function can_fetched_ignored_jobs()
+    public function can_fetch_ignored_jobs()
     {
         $crew   = $this->createCrew();
         $job    = $this->seedIgnoredJob($crew);
+
+        $this->assertCount(1, ProjectJob::all());
+        $this->assertCount(1, CrewIgnoredJob::all());
 
         $response = $this->actingAs($crew, 'api')
             ->get(route('crew.ignored.jobs'))
             ->assertSee('Successfully fetched crew\'s ignored jobs')
             ->assertSuccessful();
 
-        $this->assertCount(1, ProjectJob::all());
-        $this->assertCount(1, CrewIgnoredJob::all());
-        $response->assertJson(['jobs' => [$job->toArray()]]);
+        $response->assertJson(
+            [
+                'jobs' => [$job->toArray()]
+            ]
+        );
+    }
+
+    /**
+     * @test
+     * @covers App\Http\Controllers\API\Crew/ProjectJobController::submissions
+     */
+    public function can_fetch_submissions()
+    {
+        $crew       = $this->createCrew();
+        $submission = $this->seedNewSubmission($crew);
+
+        $response = $this->actingAs($crew, 'api')
+            ->get(route('crew.submissions'))
+            ->assertSee('Successfully fetched crew\'s submissions')
+            ->assertSuccessful();
+
+        $response->assertJson(
+            [
+                'jobs' => [$submission->toArray()]
+            ]
+        );
+    }
+    
+    /**
+     * @test
+     * @covers App\Http\Controllers\API\Crew/ProjectJobController::submissions
+     */
+    public function should_only_fetch_submissions_that_are_less_than_90_days()
+    {
+        $this->withoutExceptionHandling();
+
+        $crew           = $this->createCrew();
+        $newSubmission  = $this->seedNewSubmission($crew);
+        $oldSubmission  = $this->seedOldSubmission($crew);
+
+        $this->assertCount(2, ProjectJob::all());
+
+        $response = $this->actingAs($crew, 'api')
+            ->get(route('crew.submissions'))
+            ->assertSee('Successfully fetched crew\'s submissions')
+            ->assertSuccessful();
+
+        $response->assertJson(
+            [
+                'jobs' => [$newSubmission->toArray()]
+            ]
+        );
+        $response->assertJsonMissing(
+            [
+                'jobs' => [$oldSubmission->toArray()]
+            ]
+        );
     }
 
     /**
@@ -80,18 +138,32 @@ class ProjectJobControllerTest extends TestCase
         $crew           = $this->createCrew();
         $jobIgnored     = $this->seedIgnoredJob($crew);
         $jobOpen        = $this->seedOpenJob($crew);
-        $jobSubmission  = $this->seedSubmission($crew);
+        $jobSubmission  = $this->seedNewSubmission($crew);
+
+        $this->assertCount(3, ProjectJob::all());
+        $this->assertCount(1, CrewIgnoredJob::all());
 
         $response = $this->actingAs($crew, 'api')
             ->get(route('crew.ignored.jobs'))
             ->assertSee('Successfully fetched crew\'s ignored jobs')
             ->assertSuccessful();
 
-        $this->assertCount(3, ProjectJob::all());
-        $this->assertCount(1, CrewIgnoredJob::all());
-        $response->assertJson(['jobs' => [$jobIgnored->toArray()]]);
-        $response->assertJsonMissing(['jobs' => [$jobOpen->toArray()]]);
-        $response->assertJsonMissing(['jobs' => [$jobSubmission->toArray()]]);
+        
+        $response->assertJson(
+            [
+                'jobs' => [$jobIgnored->toArray()]
+            ]
+        );
+        $response->assertJsonMissing(
+            [
+                'jobs' => [$jobOpen->toArray()]
+            ]
+        );
+        $response->assertJsonMissing(
+            [
+                'jobs' => [$jobSubmission->toArray()]
+            ]
+        );
     }
 
      /**
@@ -119,16 +191,26 @@ class ProjectJobControllerTest extends TestCase
         $crew2          = $this->createCrew();
         $notMatchedJob  = $this->seedIgnoredJob($crew2);
 
+        $this->assertCount(2, ProjectJob::all());
+        $this->assertCount(2, CrewIgnoredJob::all());
+        $this->assertCount(2, CrewPosition::all());
+
         $response = $this->actingAs($crew, 'api')
             ->get(route('crew.ignored.jobs'))
             ->assertSee('Successfully fetched crew\'s ignored jobs')
             ->assertSuccessful();
         
-        $this->assertCount(2, ProjectJob::all());
-        $this->assertCount(2, CrewIgnoredJob::all());
-        $this->assertCount(2, CrewPosition::all());
-        $response->assertJson(['jobs' => [$jobIgnored->toArray()]]);
-        $response->assertJsonMissing(['jobs' => [$notMatchedJob->toArray()]]);
+        
+        $response->assertJson(
+            [
+                'jobs' => [$jobIgnored->toArray()]
+            ]
+        );
+        $response->assertJsonMissing(
+            [
+                'jobs' => [$notMatchedJob->toArray()]
+            ]
+        );
     }
 
     private function seedOpenJob($crew)
@@ -146,15 +228,17 @@ class ProjectJobControllerTest extends TestCase
         $position   = $this->createCrewPosition($crew->id);
         $job        = $this->createProjectJob($project, $position);
         
-        factory(CrewIgnoredJob::class)->create([
-            'crew_id'        => $crew->id,
-            'project_job_id' => $job->id
-        ]);
+        factory(CrewIgnoredJob::class)->create(
+            [
+                'crew_id'        => $crew->id,
+                'project_job_id' => $job->id
+            ]
+        );
 
         return $job;
     }
 
-    private function seedSubmission($crew)
+    private function seedNewSubmission($crew)
     {
         $project    = factory(Project::class)->create();
         $position   = $this->createCrewPosition($crew->id);
@@ -171,25 +255,49 @@ class ProjectJobControllerTest extends TestCase
         return $job;
     }
 
+    private function seedOldSubmission($crew)
+    {
+        $project    = factory(Project::class)->create();
+        $position   = $this->createCrewPosition($crew->id);
+        $job        = $this->createProjectJob($project, $position);
+
+        factory(Submission::class)->create(
+            [
+                'crew_id'          => $crew->id,
+                'project_id'       => $project->id,
+                'project_job_id'   => $job->id,
+                'created_at'       => Carbon::now()->subDays(91),
+            ]
+        );
+
+        return $job;
+    }
+
     private function createCrewPosition($crewId)
     {
-        $position = factory(Position::class)->create([
-            'name' => 'Prosthetics/FX Makeup'
-        ]);
-        factory(CrewPosition::class)->create([
-            'crew_id'       => $crewId,
-            'position_id'   => $position->id
-        ]);
+        $position = factory(Position::class)->create(
+            [
+                'name' => 'Prosthetics/FX Makeup'
+            ]
+        );
+        factory(CrewPosition::class)->create(
+            [
+                'crew_id'       => $crewId,
+                'position_id'   => $position->id
+            ]
+        );
 
         return $position;
     }
 
     private function createProjectJob($project, $position)
     {
-        return factory(ProjectJob::class)->create([
-            'project_id'    => $project->id,
-            'position_id'   => $position->id,
-            'rush_call'     => 0
-        ]);
+        return factory(ProjectJob::class)->create(
+            [
+                'project_id'    => $project->id,
+                'position_id'   => $position->id,
+                'rush_call'     => 0
+            ]
+        );
     }
 }
